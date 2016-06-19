e293c9794c6da099d1dc6d767e0289bc47ae168b
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-06-19 00:41:36 -0700

    more work on richtext

--- examples/richtext/index.js
+++ examples/richtext/index.js
@@ -17,18 +17,7 @@ const state = {
           type: 'text',
           ranges: [
             {
-              text: 'This is '
-            },
-            {
-              text: 'editable',
-              marks: [
-                {
-                  type: 'italic'
-                }
-              ]
-            },
-            {
-              text: ' '
+              text: 'This is editable '
             },
             {
               text: 'rich',
@@ -39,7 +28,18 @@ const state = {
               ]
             },
             {
-              text: ' text, much better than a '
+              text: ' text, '
+            },
+            {
+              text: 'much',
+              marks: [
+                {
+                  type: 'italic'
+                }
+              ]
+            },
+            {
+              text: ' better than a '
             },
             {
               text: '<textarea>',
@@ -69,18 +69,77 @@ class App extends React.Component {
     state: Raw.deserialize(state)
   };
 
+  isMarkActive(type) {
+    const { state } = this.state
+    const { document, selection } = state
+    const { startKey, startOffset } = selection
+    const startNode = document.getNode(startKey)
+    if (!startNode) return false
+
+    const { characters } = startNode
+    const character = characters.get(startOffset)
+    const { marks } = character
+    return marks.some(mark => mark.type == type)
+  }
+
+  onClickMark(e, type) {
+    e.preventDefault()
+
+    let { state } = this.state
+    const { marks } = state
+    const isActive = this.isMarkActive(type)
+
+    state = state
+      .transform()
+      [isActive ? 'unmark' : 'mark']()
+      .apply()
+
+    this.onChange(state)
+  }
+
   render() {
     return (
-      <Editor
-        state={this.state.state}
-        renderNode={node => this.renderNode(node)}
-        renderMark={mark => this.renderMark(mark)}
-        onChange={(state) => {
-          console.log('Document:', state.document.toJS())
-          console.log('Content:', Raw.serialize(state))
-          this.setState({ state })
-        }}
-      />
+      <div>
+        {this.renderToolbar()}
+        {this.renderEditor()}
+      </div>
+    )
+  }
+
+  renderToolbar() {
+    const isBold = this.isMarkActive('bold')
+    const isItalic = this.isMarkActive('italic')
+    const isCode = this.isMarkActive('code')
+
+    return (
+      <div className="menu">
+        <span className="button" onClick={e => this.onClickMark(e, 'bold')} data-active={isBold}>
+          <span className="material-icons">format_bold</span>
+        </span>
+        <span className="button" onClick={e => this.onClickMark(e, 'italic')} data-active={isItalic}>
+          <span className="material-icons">format_italic</span>
+        </span>
+        <span className="button" onClick={e => this.onClickMark(e, 'code')} data-active={isCode}>
+          <span className="material-icons">code</span>
+        </span>
+      </div>
+    )
+  }
+
+  renderEditor() {
+    return (
+      <div className="editor">
+        <Editor
+          state={this.state.state}
+          renderNode={node => this.renderNode(node)}
+          renderMark={mark => this.renderMark(mark)}
+          onChange={(state) => {
+            console.log('Document:', state.document.toJS())
+            console.log('Content:', Raw.serialize(state))
+            this.setState({ state })
+          }}
+        />
+      </div>
     )
   }
 
