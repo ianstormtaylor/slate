1c68ab5d5c27bfd7403980198db6a11a843f17ae
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-06-23 09:15:51 -0700

    add split inline with tests

---
+++ test/server/transforms/fixtures/split-inline-at-range/with-delete/index.js
@@ -0,0 +1,17 @@
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTextNodes()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 1,
+    focusKey: first.key,
+    focusOffset: 3
+  })
+
+  return state
+    .transform()
+    .splitInlineAtRange(range)
+    .apply()
+}
