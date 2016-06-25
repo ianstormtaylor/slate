d9a33657a8100b1797a35e15f10d739adabb8935
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-06-24 18:01:34 -0700

    got insert fragment working

---
+++ test/transforms/fixtures/insert-fragment-at-range/middle-inline/index.js
@@ -0,0 +1,25 @@
+
+import path from 'path'
+import readMetadata from 'read-metadata'
+import { Raw } from '../../../../..'
+
+export default function (state) {
+  const file = path.resolve(__dirname, 'fragment.yaml')
+  const raw = readMetadata.sync(file)
+  const fragment = Raw.deserialize(raw).document
+
+  const { document, selection } = state
+  const texts = document.getTextNodes()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: first.key,
+    focusOffset: 2
+  })
+
+  return state
+    .transform()
+    .insertFragmentAtRange(range, fragment)
+    .apply()
+}
