7e5642d8484ab0a1184f320b6addf22ac37446c7
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-14 14:34:04 -0700

    add ability to turn off collapsed cursor marks, closes #88

---
+++ test/transforms/fixtures/mark/collapsed-selection-off-again/index.js
@@ -0,0 +1,20 @@
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTextNodes()
+  const first = texts.first()
+
+  return state
+    .transform()
+    .moveTo({
+      anchorKey: first.key,
+      anchorOffset: 0,
+      focusKey: first.key,
+      focusOffset: 0
+    })
+    .mark('bold')
+    .insertText('a')
+    .unmark('bold')
+    .insertText('b')
+    .apply()
+}
