9c020d9a04b847bd95786e149f55229e6e8b5475
Julian Krispel-Samsel <julian@goodafternoon.co>
2016-07-16 01:02:03 +0200

    Typos (#95)

--- examples/rich-text/index.js
+++ examples/rich-text/index.js
@@ -12,10 +12,10 @@ import keycode from 'keycode'
 
 const NODES = {
   'block-quote': props => <blockquote {...props.attributes}>{props.children}</blockquote>,
-  'bulleted-list': props => <ul {...props.attributes}>{props.chidlren}</ul>,
+  'bulleted-list': props => <ul {...props.attributes}>{props.children}</ul>,
   'heading-one': props => <h1 {...props.attributes}>{props.children}</h1>,
   'heading-two': props => <h2 {...props.attributes}>{props.children}</h2>,
-  'list-item': props => <li {...props.attributes}>{props.chidlren}</li>,
+  'list-item': props => <li {...props.attributes}>{props.children}</li>,
   'numbered-list': props => <ol {...props.attributes}>{props.children}</ol>
 }
 
