00261eded91288d5f15a20d873f8664383d587d4
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 12:26:11 -0700

    add toggleMarkAtRange remove tests

---
+++ test/transforms/fixtures/toggle-mark-at-range/remove-across-blocks/index.js
@@ -0,0 +1,18 @@
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const second = texts.last()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: second.key,
+    focusOffset: 2
+  })
+
+  return state
+    .transform()
+    .toggleMarkAtRange(range, 'bold')
+    .apply()
+}
