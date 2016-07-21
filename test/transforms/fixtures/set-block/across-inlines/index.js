37829137c664a4d1eb0dcfcacc6d87de06dd342d
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 15:15:50 -0700

    add setBlock and setInline transform tests

---
+++ test/transforms/fixtures/set-block/across-inlines/index.js
@@ -0,0 +1,28 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const second = texts.last()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: second.key,
+    focusOffset: 2
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .setBlock({ type: 'code' })
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.toJS()
+  )
+
+  return next
+}
---
+++ test/transforms/fixtures/set-block/with-data/index.js
@@ -0,0 +1,31 @@
+
+import { Data } from '../../../../..'
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 0,
+    focusKey: first.key,
+    focusOffset: 0
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .setBlock({
+      type: 'code',
+      data: Data.create({ key: 'value' })
+    })
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.toJS()
+  )
+
+  return next
+}
