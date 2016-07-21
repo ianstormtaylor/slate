986e49aaac1f9e2ada2689b4e94f39c0efd73268
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 15:34:41 -0700

    add splitBlock and splitInline transforms tests

---
+++ test/transforms/fixtures/split-inline/depth/index.js
@@ -0,0 +1,29 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: first.key,
+    focusOffset: 2
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .splitInline(1)
+    .apply()
+
+  const updated = next.document.getTexts().last()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.collapseToStartOf(updated).toJS()
+  )
+
+  return next
+}
