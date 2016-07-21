0dc348dec8af1611014bb37b10bc2263147b5a9e
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 16:08:12 -0700

    add insertText transform tests

---
+++ test/transforms/fixtures/insert-text/middle-character/index.js
@@ -0,0 +1,27 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 1,
+    focusKey: first.key,
+    focusOffset: 1
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .insertText('a')
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.moveForward().toJS()
+  )
+
+  return next
+}
