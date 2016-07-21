1bf729db5f6d432d10ebc23823098f8205f8ed37
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 15:50:08 -0700

    add wrapBlock transform tests

---
+++ test/transforms/fixtures/wrap-block/across-inlines/index.js
@@ -0,0 +1,28 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const second = texts.last()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: second.key,
+    focusOffset: 2
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .wrapBlock('quote')
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.toJS()
+  )
+
+  return next
+}
