035ae4451aaaca2ca014b21208516fcdeb22ac68
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 11:59:39 -0700

    add more mark tests

---
+++ test/transforms/fixtures/add-mark/with-mark-object/index.js
@@ -0,0 +1,22 @@
+
+import { Mark } from '../../../../..'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+
+  return state
+    .transform()
+    .moveTo({
+      anchorKey: first.key,
+      anchorOffset: 0,
+      focusKey: first.key,
+      focusOffset: 1
+    })
+    .addMark(Mark.create({
+      type: 'bold',
+      data: { key: 'value' }
+    }))
+    .apply()
+}
