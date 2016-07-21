7615ce8611cd936e6fc53e27850912aabb092802
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 14:57:17 -0700

    add insertFragment transform tests

---
+++ test/transforms/fixtures/insert-fragment/start-second-block/index.js
@@ -0,0 +1,42 @@
+
+import assert from 'assert'
+import path from 'path'
+import readMetadata from 'read-metadata'
+import { Raw } from '../../../../..'
+
+export default function (state) {
+  const file = path.resolve(__dirname, 'fragment.yaml')
+  const raw = readMetadata.sync(file)
+  const fragment = Raw.deserialize(raw).document
+
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const second = texts.get(1)
+  const last = fragment.getTexts().last()
+  const range = selection.merge({
+    anchorKey: second.key,
+    anchorOffset: 0,
+    focusKey: second.key,
+    focusOffset: 0
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .insertFragment(fragment)
+    .apply()
+
+  const updated = next.document.getTexts().get(1)
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.merge({
+      anchorKey: updated.key,
+      anchorOffset: last.length,
+      focusKey: updated.key,
+      focusOffset: last.length
+    }).toJS()
+  )
+
+  return next
+}
