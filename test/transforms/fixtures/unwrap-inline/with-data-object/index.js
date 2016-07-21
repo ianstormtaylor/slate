dd189f73854cda31d800945358b0c9d467537634
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 15:48:05 -0700

    add unwrapInline transform tests

---
+++ test/transforms/fixtures/unwrap-inline/with-data-object/index.js
@@ -0,0 +1,27 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 1,
+    focusKey: first.key,
+    focusOffset: 3
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .unwrapInline('hashtag', { key: 'one' })
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.toJS()
+  )
+
+  return next
+}
