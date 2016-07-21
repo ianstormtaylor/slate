eb95b79a004d03c4b1d0d4046a5a5d86d76ebf6e
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-21 16:03:02 -0700

    add wrapInline transform tests

---
+++ test/transforms/fixtures/wrap-inline/across-inlines/index.js
@@ -0,0 +1,36 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const second = texts.last()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: second.key,
+    focusOffset: 2
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .wrapInline('hashtag')
+    .apply()
+
+  const two = next.document.getTexts().get(1)
+  const three = next.document.getTexts().get(2)
+
+  // assert.deepEqual(
+  //   next.selection.toJS(),
+  //   range.merge({
+  //     anchorKey: two.key,
+  //     anchorOffset: 0,
+  //     focusKey: three.key,
+  //     focusOffset: three.length
+  //   }).toJS()
+  // )
+
+  return next
+}
---
+++ test/transforms/fixtures/wrap-inline/with-data/index.js
@@ -0,0 +1,35 @@
+
+import { Data } from '../../../../..'
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 1,
+    focusKey: first.key,
+    focusOffset: 3
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .wrapInline('hashtag', Data.create({ key: 'value' }))
+    .apply()
+
+  const updated = next.document.getTexts().get(1)
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.merge({
+      anchorKey: updated.key,
+      anchorOffset: 0,
+      focusKey: updated.key,
+      focusOffset: updated.length
+    }).toJS()
+  )
+
+  return next
+}
