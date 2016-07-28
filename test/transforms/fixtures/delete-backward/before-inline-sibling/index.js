fc8fb18fdabd08500a94977bf8c52d762150a172
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-07-27 17:09:10 -0700

    fix deleting sibling text node

---
+++ test/transforms/fixtures/delete-backward/before-inline-sibling/index.js
@@ -0,0 +1,29 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const last = texts.last()
+  const range = selection.merge({
+    anchorKey: last.key,
+    anchorOffset: last.length,
+    focusKey: last.key,
+    focusOffset: last.length
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .deleteBackward()
+    .apply()
+
+  const updated = next.document.getTexts().last()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.collapseToEndOf(updated).toJS()
+  )
+
+  return next
+}
---
+++ test/transforms/fixtures/delete/before-inline-sibling/index.js
@@ -0,0 +1,34 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const last = texts.last()
+  const range = selection.merge({
+    anchorKey: last.key,
+    anchorOffset: last.length - 1,
+    focusKey: last.key,
+    focusOffset: last.length
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .delete()
+    .apply()
+
+  const updated = next.document.getTexts().last()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.merge({
+      anchorKey: updated.key,
+      anchorOffset: updated.length,
+      focusKey: updated.key,
+      focusOffset: updated.length
+    }).toJS()
+  )
+
+  return next
+}
