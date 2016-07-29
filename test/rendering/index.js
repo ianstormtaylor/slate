845e284ee4820e30e815b14545302746a2ce5e00
Samy Pessé <samypesse@gmail.com>
2016-07-29 20:25:07 +0200

    Add RTL support (#204)
    * Add property textAlignment for the whole editor
    
    * textAlignment should be set on Content
    
    * Add basic for directionMap in State
    
    * Add RTL to example
    
    * Use setDocument for updating direction map when inserting blocks
    
    * Remove debug log
    
    * Only add 'dir' to node when is not default value
    
    * Prevent tests from failing because of .DS_Store on OSX
    
    * Add tests for rtl rendering
    
    * Fix rendering tests
    
    * Fix test for rendering "text-direction"
    
    * Remove textAlignment prop on editor
    
    * Don't use a directionMap but directly a "textDir" on nodes
    
    * Remove .setDocument from State

--- test/rendering/index.js
+++ test/rendering/index.js
@@ -16,6 +16,8 @@ describe('rendering', () => {
   const tests = fs.readdirSync(resolve(__dirname, './fixtures'))
 
   for (const test of tests) {
+    if (test[0] === '.') continue
+
     it(test, () => {
       const dir = resolve(__dirname, './fixtures', test)
       const input = readMetadata.sync(resolve(dir, 'input.yaml'))
