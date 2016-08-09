10cfb6e48b39a11189dba614c330112d1786ee98
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-08-09 23:10:03 +0700

    Change concat into insertChildrenAfter and insertChildrenBefore (#232)
    * Change concat into insertChildrenAfter and insertChildrenBefore
    
    * Update input and out yaml of insert block at range block-end
    
    * Update input and out yaml of insert block at range block-start

--- test/transforms/fixtures/insert-block-at-range/block-start/index.js
+++ test/transforms/fixtures/insert-block-at-range/block-start/index.js
@@ -2,7 +2,7 @@
 export default function (state) {
   const { document, selection } = state
   const texts = document.getTexts()
-  const first = texts.first()
+  const first = texts.last()
   const range = selection.merge({
     anchorKey: first.key,
     anchorOffset: 0,
