6ab2d963cde98d01c3e96c210923e8a17755c198
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-08-11 12:28:41 -0700

    fix html serializer for cases without children

---
+++ test/serializers/fixtures/html/deserialize/inline-no-children/index.js
@@ -0,0 +1,25 @@
+
+export default {
+  rules: [
+    {
+      deserialize(el, next) {
+        switch (el.tagName) {
+          case 'p': {
+            return {
+              kind: 'block',
+              type: 'paragraph',
+              nodes: next(el.children)
+            }
+          }
+          case 'a': {
+            return {
+              kind: 'inline',
+              type: 'link',
+              nodes: next(el.children)
+            }
+          }
+        }
+      }
+    }
+  ]
+}
