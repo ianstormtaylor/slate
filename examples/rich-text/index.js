eeb97c0611d71192a4fb3a85e1426cdad6e0e67f
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-08-13 16:48:21 -0700

    add rendering of components from schema

--- examples/rich-text/index.js
+++ examples/rich-text/index.js
@@ -10,18 +10,20 @@ import initialState from './state.json'
 const DEFAULT_NODE = 'paragraph'
 
 /**
- * Define a set of node renderers.
+ * Define a schema.
  *
  * @type {Object}
  */
 
-const NODES = {
-  'block-quote': (props) => <blockquote {...props.attributes}>{props.children}</blockquote>,
-  'bulleted-list': props => <ul {...props.attributes}>{props.children}</ul>,
-  'heading-one': props => <h1 {...props.attributes}>{props.children}</h1>,
-  'heading-two': props => <h2 {...props.attributes}>{props.children}</h2>,
-  'list-item': props => <li {...props.attributes}>{props.children}</li>,
-  'numbered-list': props => <ol {...props.attributes}>{props.children}</ol>
+const schema = {
+  nodes: {
+    'block-quote': props => <blockquote {...props.attributes}>{props.children}</blockquote>,
+    'bulleted-list': props => <ul {...props.attributes}>{props.children}</ul>,
+    'heading-one': props => <h1 {...props.attributes}>{props.children}</h1>,
+    'heading-two': props => <h2 {...props.attributes}>{props.children}</h2>,
+    'list-item': props => <li {...props.attributes}>{props.children}</li>,
+    'numbered-list': props => <ol {...props.attributes}>{props.children}</ol>,
+  }
 }
 
 /**
@@ -302,25 +304,14 @@ class RichText extends React.Component {
       <div className="editor">
         <Editor
           placeholder={'Enter some rich text...'}
+          schema={schema}
           state={this.state.state}
-          renderNode={this.renderNode}
           renderMark={this.renderMark}
           onChange={this.onChange}
           onKeyDown={this.onKeyDown}
         />
       </div>
     )
-  }
-
-  /**
-   * Return a node renderer for a Slate `node`.
-   *
-   * @param {Node} node
-   * @return {Component or Void}
-   */
-
-  renderNode = (node) => {
-    return NODES[node.type]
   }
 
   /**
