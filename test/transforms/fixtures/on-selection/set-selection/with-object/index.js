16617061eeda9fe01a8198f12f1ecc2023a64cb2
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-08-29 15:01:55 -0700

    refactor transform tests, add selection transform tests

---
+++ test/transforms/fixtures/on-selection/set-selection/with-object/index.js
@@ -0,0 +1,23 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { startText, selection } = state
+  const props = {
+    anchorKey: startText.key,
+    anchorOffset: 0,
+    focusKey: startText.key,
+    focusOffset: startText.length,
+  }
+
+  const sel = selection.merge(props)
+  const next = state
+    .transform()
+    .setSelection(props)
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    sel.toJS()
+  )
+}
