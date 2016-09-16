3bcf97c66e39be8f8ccb8eecc5e4cb3f3c74cf49
Benjamin Kniffler <bkniffler@me.com>
2016-09-16 14:58:07 +0200

    Handle empty startKey in models/node.js, e.g. after removing nodes

--- src/models/node.js
+++ src/models/node.js
@@ -619,7 +619,7 @@ const Node = {
     const marks = Mark.createSet()
 
     // If the range is collapsed at the start of the node, check the previous.
-    if (range.isCollapsed && startOffset == 0) {
+    if (range.isCollapsed && startOffset == 0 && startKey) {
       const text = this.getDescendant(startKey)
       const previous = this.getPreviousText(startKey)
       if (!previous || !previous.length) return marks
@@ -628,7 +628,7 @@ const Node = {
     }
 
     // If the range is collapsed, check the character before the start.
-    if (range.isCollapsed) {
+    if (range.isCollapsed && startKey) {
       const text = this.getDescendant(startKey)
       const char = text.characters.get(range.startOffset - 1)
       return char.marks
@@ -892,6 +892,9 @@ const Node = {
     range = range.normalize(this)
     const { startKey, endKey } = range
     const texts = this.getTexts()
+    if (!startKey || !endKey) {
+      return texts
+    }
     const startText = this.getDescendant(startKey)
     const endText = this.getDescendant(endKey)
     const start = texts.indexOf(startText)
