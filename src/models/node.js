812bb8ca873c0e9a729478ad6024bf4cd92ec75d
Samy Pesse <samypesse@gmail.com>
2016-09-20 10:18:00 +0200

    Fix indentation

--- src/models/node.js
+++ src/models/node.js
@@ -1067,7 +1067,7 @@ const Node = {
             removals = removals.add(next.key)
             next = node.getNextSibling(next)
           }
-       }
+        }
 
         // ...that there are no extra empty text nodes.
         else if (desc.length == 0) {
