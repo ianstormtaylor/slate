01bf2f766d3679bc595aefc4d8f4cfb1d3bf51da
Soreine <soreine.plume@gmail.com>
2016-10-05 15:38:37 +0200

    Avoid memoization between benchmark runs

--- perf/index.js
+++ perf/index.js
@@ -16,21 +16,37 @@ let suite = new Benchmark.Suite()
 const suiteDir = resolve(__dirname, './benchmarks')
 const benchmarks = fs.readdirSync(suiteDir)
 
-// For each benchmark to benchmark
+// For each benchmark
 for (const benchmark of benchmarks) {
   if (benchmark[0] == '.') continue
 
   const benchmarkDir = resolve(suiteDir, benchmark)
-  const fn = require(benchmarkDir)
-  const input = readMetadata.sync(resolve(benchmarkDir, 'input.yaml'))
+  // Because Benchmark does not support scoped variables well, use globals....
+  g_fun = require(benchmarkDir)
+  g_input = readMetadata.sync(resolve(benchmarkDir, 'input.yaml'))
+  g_Raw = Raw
 
-  let state = Raw.deserialize(input, { terse: true })
-
-  // add tests
+  // Add it to the benchmark suite
   suite.add({
     name: benchmark,
-    fn: () => {
-      return fn(state)
+
+    // Time spent in setup is not taken into account
+    setup() {
+      // Create as much independant Slate.State as needed, to avoid
+      // memoization between calls to `fn`
+      let states = []
+      let numberOfExecution = this.count
+      while (numberOfExecution--) {
+        states.push(g_Raw.deserialize(g_input, { terse: true }))
+      }
+      let current = 0
+    },
+
+    fn() {
+      // Because of the way Benchmark compiles the functions,
+      // the variables declared in `setup` are visible to `fn`
+      g_fun(states[current])
+      current++
     }
   })
 }
