30667882c470f69a98ed9e944fb27e548c2fb30c
Samy Pesse <samypesse@gmail.com>
2016-10-17 20:23:53 +0200

    Add test to unwrapInline only one in the document

---
+++ test/transforms/fixtures/at-current-range/unwrap-inline/only-one/index.js
@@ -0,0 +1,28 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.get(2)
+  const last = texts.get(3)
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 2,
+    focusKey: last.key,
+    focusOffset: 2
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .unwrapInline('hashtag')
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.toJS()
+  )
+
+  return next
+}
