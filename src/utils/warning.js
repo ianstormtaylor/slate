1dbc90604d7afed4a94648e4bce3b81f6cf2add9
Samy Pesse <samypesse@gmail.com>
2016-10-22 00:36:42 +0200

    Add utils/warning.js

---
+++ src/utils/warning.js
@@ -0,0 +1,29 @@
+const __DEV__ = (
+  typeof process !== 'undefined' &&
+  process.env &&
+  process.env.NODE_ENV !== 'production'
+)
+
+/**
+ * Log a development warning.
+ * @param {String} message
+ */
+
+export default function warning(message) {
+  if (!__DEV__) {
+    return
+  }
+
+  if (typeof console !== 'undefined') {
+    console.error('Warning: ', message) // eslint-disable-line no-console
+  }
+
+  try {
+      // --- Welcome to debugging Slate ---
+      // This error was thrown as a convenience so that you can use this stack
+      // to find the callsite that caused this warning to fire.
+      throw new Error(message)
+    } catch (x) {
+        // This error is only for debugging
+    }
+}
