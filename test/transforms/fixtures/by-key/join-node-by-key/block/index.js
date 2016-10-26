947513fc5852b45c521f0cdc1bc3066a46a9456e
Soreine <soreine.plume@gmail.com>
2016-10-26 14:26:55 +0200

    Add test to `joinNodeByKey`

---
+++ test/transforms/fixtures/by-key/join-node-by-key/block/index.js
@@ -0,0 +1,29 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { selection } = state
+
+  const range = selection.merge({
+    anchorKey: 'anchor',
+    anchorOffset: 1,
+    focusKey: 'focus',
+    focusOffset: 2
+  })
+
+  const next = state
+    .transform()
+    .moveTo(range)
+    .joinNodeByKey('key2', 'key1')
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    range.merge({
+      focusKey: 'anchor',
+      focusOffset: 5
+    }).toJS()
+  )
+
+  return next
+}
