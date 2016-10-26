e08ef88d540797884938ef8ef9280c20617e9eba
Samy Pessé <samypesse@gmail.com>
2016-10-26 13:42:48 +0200

    Complete tests for selection and insertTextByKey

---
+++ test/transforms/fixtures/by-key/insert-text-by-key/selection-end/index.js
@@ -0,0 +1,28 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const firstText = texts.first()
+
+  const nextSelection = selection.merge({
+    anchorKey: firstText.key,
+    focusKey: firstText.key,
+    anchorOffset: 2,
+    focusOffset: 4
+  })
+
+  const next = state
+    .transform()
+    .moveTo(nextSelection)
+    .insertTextByKey(firstText.key, 4, 'XX')
+    .apply()
+
+  assert.deepEqual(
+    next.selection.toJS(),
+    nextSelection.toJS()
+  )
+
+  return next
+}
