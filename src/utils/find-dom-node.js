36670d0d4137e9984d50a31935e979ec426e1070
Ian Storm Taylor <ian@ianstormtaylor.com>
2016-12-07 21:34:46 -0800

    update error message

--- src/utils/find-dom-node.js
+++ src/utils/find-dom-node.js
@@ -10,7 +10,7 @@ function findDOMNode(node) {
   const el = window.document.querySelector(`[data-key="${node.key}"]`)
 
   if (!el) {
-    throw new Error(`Unable to find a dom node for "${node.key}". This is
+    throw new Error(`Unable to find a DOM node for "${node.key}". This is
 often because of forgetting to add \`props.attributes\` to a component
 returned from \`renderNode\`.`)
   }
