cab29ca19c6e7c6557e8354951b32bea6b1b443b
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-02-07 12:25:53 -0800

    fix deleteing by word or character

---
+++ test/transforms/fixtures/at-range/delete-word-forward-at-range/middle-of-word/index.js
@@ -0,0 +1,17 @@
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 1,
+    focusKey: first.key,
+    focusOffset: 1,
+  })
+
+  return state
+    .transform()
+    .deleteWordForwardAtRange(range)
+    .apply()
+}
