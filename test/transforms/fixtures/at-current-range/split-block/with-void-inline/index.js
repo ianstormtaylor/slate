1f7676191cfb3609210fbb17d25c17d53988d498
Kelly Joseph Price <kelly@collaborativedrug.com>
2017-04-28 15:10:52 -0700

    Add failing spec

---
+++ test/transforms/fixtures/at-current-range/split-block/with-void-inline/index.js
@@ -0,0 +1,22 @@
+
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const { nodes } = document
+  const second = nodes.first().nodes.get(2)
+  const range = selection.merge({
+    anchorKey: second.key,
+    anchorOffset: 0,
+    focusKey: second.key,
+    focusOffset: 0
+  })
+
+  const next = state
+    .transform()
+    .select(range)
+    .splitBlock()
+    .apply()
+
+  return next
+}
