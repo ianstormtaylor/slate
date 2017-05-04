13a99a9b73cacaf366a74e28022dd65ab30a41ea
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-05-04 16:48:42 -0700

    refactor examples, upgrade dependencies

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, IndexRedirect, hashHistory } from 'react-router'
-
-/**
- * Examples.
- */
+import { HashRouter, NavLink, Route, Redirect, Switch } from 'react-router-dom'
 
 import CheckLists from './check-lists'
 import CodeHighlighting from './code-highlighting'
@@ -15,7 +11,6 @@ import FocusBlur from './focus-blur'
 import HoveringMenu from './hovering-menu'
 import Iframes from './iframes'
 import Images from './images'
-import LargeDocument from './large-document'
 import Links from './links'
 import MarkdownPreview from './markdown-preview'
 import MarkdownShortcuts from './markdown-shortcuts'
@@ -27,118 +22,84 @@ import ReadOnly from './read-only'
 import RichText from './rich-text'
 import Tables from './tables'
 
-import DevPerformancePlain from './development/performance-plain'
-import DevPerformanceRich from './development/performance-rich'
+import DevLargeDocument from './dev/large-document'
+import DevPerformancePlain from './dev/performance-plain'
+import DevPerformanceRich from './dev/performance-rich'
 
 /**
- * Perf.
+ * Environment.
+ *
+ * @type {String}
  */
 
-import Perf from 'react-addons-perf'
-
-window.Perf = Perf
+const { NODE_ENV } = process.env
 
 /**
- * Define our example app.
+ * Examples.
  *
- * @type {Component} App
+ * @type {Array}
+ */
+
+const EXAMPLES = [
+  ['Rich Text', RichText, '/rich-text'],
+  ['Plain Text', PlainText, '/plain-text'],
+  ['Hovering Menu', HoveringMenu, '/hovering-menu'],
+  ['Links', Links, '/links'],
+  ['Images', Images, '/images'],
+  ['Embeds', Embeds, '/embeds'],
+  ['Emojis', Emojis, '/emojis'],
+  ['Markdown Preview', MarkdownPreview, '/markdown-preview'],
+  ['Markdown Shortcuts', MarkdownShortcuts, '/markdown-shortcuts'],
+  ['Check Lists', CheckLists, '/check-lists'],
+  ['Code Highlighting', CodeHighlighting, '/code-highlighting'],
+  ['Tables', Tables, '/tables'],
+  ['Paste HTML', PasteHtml, '/paste-html'],
+  ['Read-only', ReadOnly, '/read-only'],
+  ['RTL', RTL, '/rtl'],
+  ['Plugins', Plugins, '/plugins'],
+  ['Iframes', Iframes, '/iframes'],
+  ['Focus & Blur', FocusBlur, '/focus-blur'],
+
+  ['DEV:Large', DevLargeDocument, '/dev-large', true],
+  ['DEV:Plain', DevPerformancePlain, '/dev-performance-plain', true],
+  ['DEV:Rich', DevPerformanceRich, '/dev-performance-rich', true],
+]
+
+/**
+ * App.
+ *
+ * @type {Component}
  */
 
 class App extends React.Component {
 
-  /**
-   * Render the example app.
-   *
-   * @return {Element} element
-   */
-
   render() {
     return (
       <div className="app">
-        {this.renderTopBar()}
-        {this.renderTabBar()}
-        {this.renderExample()}
-      </div>
-    )
-  }
-
-  /**
-   * Render the top bar.
-   *
-   * @return {Element} element
-   */
-
-  renderTopBar() {
-    return (
-      <div className="topbar">
-        <span className="topbar-title">Slate Examples</span>
-        <div className="topbar-links">
-          <a className="topbar-link" href="https://github.com/ianstormtaylor/slate">
-            GitHub
-          </a>
-          <a className="topbar-link" href="https://docs.slatejs.org/">
-            Docs
-          </a>
+        <div className="nav">
+          <span className="nav-title">Slate Examples</span>
+          <div className="nav-links">
+            <a className="nav-link" href="https://github.com/ianstormtaylor/slate">GitHub</a>
+            <a className="nav-link" href="https://docs.slatejs.org/">Docs</a>
+          </div>
         </div>
-      </div>
-    )
-  }
-
-  /**
-   * Render the tab bar.
-   *
-   * @return {Element} element
-   */
-
-  renderTabBar() {
-    return (
-      <div className="tabs">
-        {this.renderTab('Rich Text', 'rich-text')}
-        {this.renderTab('Plain Text', 'plain-text')}
-        {this.renderTab('Hovering Menu', 'hovering-menu')}
-        {this.renderTab('Links', 'links')}
-        {this.renderTab('Images', 'images')}
-        {this.renderTab('Embeds', 'embeds')}
-        {this.renderTab('Emojis', 'emojis')}
-        {this.renderTab('Markdown Preview', 'markdown-preview')}
-        {this.renderTab('Markdown Shortcuts', 'markdown-shortcuts')}
-        {this.renderTab('Check Lists', 'check-lists')}
-        {this.renderTab('Code Highlighting', 'code-highlighting')}
-        {this.renderTab('Tables', 'tables')}
-        {this.renderTab('Paste HTML', 'paste-html')}
-        {this.renderTab('Read-only', 'read-only')}
-        {this.renderTab('RTL', 'rtl')}
-        {this.renderTab('Plugins', 'plugins')}
-        {this.renderTab('Iframes', 'iframes')}
-        {this.renderTab('Focus & Blur', 'focus-blur')}
-        {this.renderTab('Large Document', 'large')}
-      </div>
-    )
-  }
-
-  /**
-   * Render a tab with `name` and `slug`.
-   *
-   * @param {String} name
-   * @param {String} slug
-   */
-
-  renderTab(name, slug) {
-    return (
-      <Link className="tab" activeClassName="active" to={slug}>{name}</Link>
-    )
-  }
-
-  /**
-   * Render the example.
-   *
-   * @return {Element} element
-   */
-
-  renderExample() {
-    return (
-      <div className="example">
-        {this.props.children}
+        <div className="tabs">
+          {EXAMPLES.map(([ name, Component, path, isDev ]) => (
+            (NODE_ENV != 'production' || !isDev) && (
+              <NavLink key={path} to={path} className="tab"activeClassName="active">
+                {name}
+              </NavLink>
+            )
+          ))}
+        </div>
+        <div className="example">
+          <Switch>
+            {EXAMPLES.map(([ name, Component, path, isDev ]) => (
+              <Route key={path} path={path} component={Component} />
+            ))}
+            <Redirect from="/" to="/rich-text" />
+          </Switch>
+        </div>
       </div>
     )
   }
@@ -151,36 +112,15 @@ class App extends React.Component {
  * @type {Element} router
  */
 
-const router = (
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRedirect to="rich-text" />
+const router = <HashRouter><App /></HashRouter>
 
-      <Route path="check-lists" component={CheckLists} />
-      <Route path="code-highlighting" component={CodeHighlighting} />
-      <Route path="embeds" component={Embeds} />
-      <Route path="emojis" component={Emojis} />
-      <Route path="focus-blur" component={FocusBlur} />
-      <Route path="hovering-menu" component={HoveringMenu} />
-      <Route path="iframes" component={Iframes} />
-      <Route path="images" component={Images} />
-      <Route path="large" component={LargeDocument} />
-      <Route path="links" component={Links} />
-      <Route path="markdown-preview" component={MarkdownPreview} />
-      <Route path="markdown-shortcuts" component={MarkdownShortcuts} />
-      <Route path="paste-html" component={PasteHtml} />
-      <Route path="plain-text" component={PlainText} />
-      <Route path="plugins" component={Plugins} />
-      <Route path="read-only" component={ReadOnly} />
-      <Route path="rich-text" component={RichText} />
-      <Route path="rtl" component={RTL} />
-      <Route path="tables" component={Tables} />
+/**
+ * Attach `Perf` when not in production.
+ */
 
-      <Route path="dev-performance-plain" component={DevPerformancePlain} />
-      <Route path="dev-performance-rich" component={DevPerformanceRich} />
-    </Route>
-  </Router>
-)
+if (NODE_ENV != 'production') {
+  window.Perf = require('react-addons-perf')
+}
 
 /**
  * Mount the router.
