142ba2ba1891f55b7f4a7cd3cb1ba979af156a57
Max Stoiber <contact@mxstbr.com>
2017-05-05 20:46:44 +0200

    Add tests for plugins (#792)
    * Add tests for plugins
    
    The first test fails because the rules property of plugins doesn't work
    as expected.
    
    * Fix plugins tes

---
+++ test/plugins/index.js
@@ -0,0 +1,65 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/server'
+import assert from 'assert'
+import cheerio from 'cheerio'
+import fs from 'fs-promise'
+import readYaml from 'read-yaml-promise'
+import { Editor, Raw } from '../..'
+import { resolve } from 'path'
+
+/**
+ * Tests.
+ */
+
+describe.only('plugins', () => {
+  const tests = fs.readdirSync(resolve(__dirname, './fixtures'))
+
+  for (const test of tests) {
+    if (test[0] === '.') continue
+
+    it(test, async () => {
+      const dir = resolve(__dirname, './fixtures', test)
+      const input = await readYaml(resolve(dir, 'input.yaml'))
+      const output = await fs.readFile(resolve(dir, 'output.html'), 'utf8')
+      const props = {
+        state: Raw.deserialize(input, { terse: true }),
+        onChange: () => {},
+        ...require(dir)
+      }
+
+      const string = ReactDOM.renderToStaticMarkup(<Editor {...props} />)
+      const expected = cheerio
+        .load(output)
+        .html()
+        .trim()
+        .replace(/\n/gm, '')
+        .replace(/>\s*</g, '><')
+
+      assert.equal(clean(string), expected)
+    })
+  }
+})
+
+/**
+ * Clean a renderer `html` string, removing dynamic attributes.
+ *
+ * @param {String} html
+ * @return {String}
+ */
+
+function clean(html) {
+  const $ = cheerio.load(html)
+
+  $('*').each((i, el) => {
+    $(el).removeAttr('data-key')
+    $(el).removeAttr('data-offset-key')
+  })
+
+  $.root().children().removeAttr('autocorrect')
+  $.root().children().removeAttr('spellcheck')
+  $.root().children().removeAttr('style')
+  $.root().children().removeAttr('data-gramm')
+
+  return $.html()
+}
