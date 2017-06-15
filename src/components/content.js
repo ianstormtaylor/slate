54b04a8e555dd37576b014af86a7e35064eab1f1
jian <yujianok@gmail.com>
2017-06-15 15:12:45 +0800

    fix IME input will replace characters after cursor

--- src/components/content.js
+++ src/components/content.js
@@ -266,7 +266,7 @@ class Content extends React.Component {
     if (this.props.readOnly) return
     if (!this.isInEditor(event.target)) return
 
-    const data = {}
+    const data = { isComposing: this.tmp.isComposing }
 
     debug('onBeforeInput', { event, data })
     this.props.onBeforeInput(event, data)
