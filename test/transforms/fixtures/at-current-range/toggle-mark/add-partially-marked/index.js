8aab26257e5007da9e9ae38358816e5a6814b6b8
Dan Burzo <danburzo@gmail.com>
2017-08-15 13:30:12 +0300

    Added test for toggleMark add in partially marked selection, with and without other marks

---
+++ test/transforms/fixtures/at-current-range/toggle-mark/add-partially-marked/index.js
@@ -0,0 +1,23 @@
+import assert from 'assert'
+
+export default function (state) {
+  const { document, selection } = state
+  const texts = document.getTexts()
+  const first = texts.first()
+  const range = selection.merge({
+    anchorKey: first.key,
+    anchorOffset: 0,
+    focusKey: first.key,
+    focusOffset: 5
+  })
+
+  const next = state
+    .transform()
+    .select(range)
+    .toggleMark('bold')
+    .apply()
+
+  assert.deepEqual(next.selection.toJS(), range.toJS())
+
+  return next
+}
