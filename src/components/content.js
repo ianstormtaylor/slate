423f5807a67b5eddbb74887ae48771279643b81d
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-09-07 15:58:35 -0700

    fix comments and logic more

--- src/components/content.js
+++ src/components/content.js
@@ -795,19 +795,32 @@ class Content extends React.Component {
       const focusBlock = document.getClosestBlock(focus.key)
       const anchorBlock = document.getClosestBlock(anchor.key)
 
-      // COMPAT: If the anchor point is outside a void, and the focus point is
-      // inside one, with an offset of `1`, we need to nudge the offset back to
-      // `0`. This is because of how void nodes <span>'s are positioned,
-      // resulting in the offset always being `1`. Since we can't know which,
-      // and since an offset of `0` is less destructive, since it creates a
-      // hanging selection, we choose `0` instead. (2017/09/07)
-      if (anchorBlock && !anchorBlock.isVoid && focusBlock && focusBlock.isVoid && focus.offset == 1) {
+      // COMPAT: If the anchor point is at the start of a non-void, and the
+      // focus point is inside a void node with an offset that isn't `0`, set
+      // the focus offset to `0`. This is due to void nodes <span>'s being
+      // positioned off screen, resulting in the offset always being greater
+      // than `0`. Since we can't know what it really should be, and since an
+      // offset of `0` is less destructive, since it creates a hanging
+      // selection, go with `0`. (2017/09/07)
+      if (
+        anchorBlock &&
+        !anchorBlock.isVoid &&
+        anchor.offset == 0 &&
+        focusBlock &&
+        focusBlock.isVoid &&
+        focus.offset != 0
+      ) {
         properties.focusOffset = 0
       }
 
-      // If the selection is at the end of a non-void inline node, and
-      // there is a node after it, put it in the node after instead.
-      if (anchorInline && !anchorInline.isVoid && anchor.offset == anchorText.text.length) {
+      // COMPAT: If the selection is at the end of a non-void inline node, and
+      // there is a node after it, put it in the node after instead. This
+      // standardizes the behavior, since it's indistinguishable to the user.
+      if (
+        anchorInline &&
+        !anchorInline.isVoid &&
+        anchor.offset == anchorText.text.length
+      ) {
         const block = document.getClosestBlock(anchor.key)
         const next = block.getNextText(anchor.key)
         if (next) {
@@ -816,7 +829,11 @@ class Content extends React.Component {
         }
       }
 
-      if (focusInline && !focusInline.isVoid && focus.offset == focusText.text.length) {
+      if (
+        focusInline &&
+        !focusInline.isVoid &&
+        focus.offset == focusText.text.length
+      ) {
         const block = document.getClosestBlock(focus.key)
         const next = block.getNextText(focus.key)
         if (next) {
