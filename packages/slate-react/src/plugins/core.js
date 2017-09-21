6d6c32175b770cfe110ba05e12646dc3ae034e73
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-09-21 11:50:10 -0700

    remove extra removeChild

--- packages/slate-react/src/plugins/core.js
+++ packages/slate-react/src/plugins/core.js
@@ -203,7 +203,6 @@ function Plugin(options = {}) {
     // in the HTML, and can be used for intra-Slate pasting. If it's a text
     // node, wrap it in a `<span>` so we have something to set an attribute on.
     if (attach.nodeType == 3) {
-      contents.removeChild(attach)
       const span = window.document.createElement('span')
 
       // COMPAT: In Chrome and Safari, if we don't add the `white-space` style
