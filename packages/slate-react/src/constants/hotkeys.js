bb9093376cb8f10e518773e937ce28aefebc3f65
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-10-17 18:01:15 -0700

    update hotkey helper

--- packages/slate-react/src/constants/hotkeys.js
+++ packages/slate-react/src/constants/hotkeys.js
@@ -1,16 +1,7 @@
 
-import isHotkey from 'is-hotkey'
+import { isKeyHotkey } from 'is-hotkey'
 
 import { IS_MAC } from './environment'
-
-/**
- * We need to use `byKey` for system-level shortcuts because they will change
- * based on the keyboard layout (eg. DVORAK).
- *
- * @type {Function}
- */
-
-const byKey = hotkey => isHotkey(hotkey, { byKey: true })
 
 /**
  * Hotkeys.
@@ -18,59 +9,59 @@ const byKey = hotkey => isHotkey(hotkey, { byKey: true })
  * @type {Function}
  */
 
-const BOLD = byKey('mod+b')
-const ITALIC = byKey('mod+i')
+const BOLD = isKeyHotkey('mod+b')
+const ITALIC = isKeyHotkey('mod+i')
 
-const ENTER = byKey('enter')
-const SHIFT_ENTER = byKey('shift+enter')
+const ENTER = isKeyHotkey('enter')
+const SHIFT_ENTER = isKeyHotkey('shift+enter')
 const SPLIT_BLOCK = e => ENTER(e) || SHIFT_ENTER(e)
 
-const BACKSPACE = byKey('backspace')
-const SHIFT_BACKSPACE = byKey('shift+backspace')
-const DELETE = byKey('delete')
-const SHIFT_DELETE = byKey('shift+delete')
+const BACKSPACE = isKeyHotkey('backspace')
+const SHIFT_BACKSPACE = isKeyHotkey('shift+backspace')
+const DELETE = isKeyHotkey('delete')
+const SHIFT_DELETE = isKeyHotkey('shift+delete')
 const DELETE_BACKWARD = e => BACKSPACE(e) || SHIFT_BACKSPACE(e)
 const DELETE_FORWARD = e => DELETE(e) || SHIFT_DELETE(e)
 
-const DELETE_CHAR_BACKWARD_MAC = byKey('ctrl+h')
-const DELETE_CHAR_FORWARD_MAC = byKey('ctrl+d')
+const DELETE_CHAR_BACKWARD_MAC = isKeyHotkey('ctrl+h')
+const DELETE_CHAR_FORWARD_MAC = isKeyHotkey('ctrl+d')
 const DELETE_CHAR_BACKWARD = e => DELETE_BACKWARD(e) || (IS_MAC && DELETE_CHAR_BACKWARD_MAC(e))
 const DELETE_CHAR_FORWARD = e => DELETE_FORWARD(e) || (IS_MAC && DELETE_CHAR_FORWARD_MAC(e))
 
-const DELETE_LINE_BACKWARD_MAC = byKey('cmd+backspace')
-const DELETE_LINE_FORWARD_MAC = byKey('ctrl+k')
+const DELETE_LINE_BACKWARD_MAC = isKeyHotkey('cmd+backspace')
+const DELETE_LINE_FORWARD_MAC = isKeyHotkey('ctrl+k')
 const DELETE_LINE_BACKWARD = e => IS_MAC && DELETE_LINE_BACKWARD_MAC(e)
 const DELETE_LINE_FORWARD = e => IS_MAC && DELETE_LINE_FORWARD_MAC(e)
 
-const DELETE_WORD_BACKWARD_MAC = byKey('option+backspace')
-const DELETE_WORD_BACKWARD_PC = byKey('ctrl+backspace')
-const DELETE_WORD_FORWARD_MAC = byKey('option+delete')
-const DELETE_WORD_FORWARD_PC = byKey('ctrl+delete')
+const DELETE_WORD_BACKWARD_MAC = isKeyHotkey('option+backspace')
+const DELETE_WORD_BACKWARD_PC = isKeyHotkey('ctrl+backspace')
+const DELETE_WORD_FORWARD_MAC = isKeyHotkey('option+delete')
+const DELETE_WORD_FORWARD_PC = isKeyHotkey('ctrl+delete')
 const DELETE_WORD_BACKWARD = e => IS_MAC ? DELETE_WORD_BACKWARD_MAC(e) : DELETE_WORD_BACKWARD_PC(e)
 const DELETE_WORD_FORWARD = e => IS_MAC ? DELETE_WORD_FORWARD_MAC(e) : DELETE_WORD_FORWARD_PC(e)
 
-const COLLAPSE_CHAR_FORWARD = byKey('right')
-const COLLAPSE_CHAR_BACKWARD = byKey('left')
+const COLLAPSE_CHAR_FORWARD = isKeyHotkey('right')
+const COLLAPSE_CHAR_BACKWARD = isKeyHotkey('left')
 
-const COLLAPSE_LINE_BACKWARD_MAC = byKey('option+up')
-const COLLAPSE_LINE_FORWARD_MAC = byKey('option+down')
+const COLLAPSE_LINE_BACKWARD_MAC = isKeyHotkey('option+up')
+const COLLAPSE_LINE_FORWARD_MAC = isKeyHotkey('option+down')
 const COLLAPSE_LINE_BACKWARD = e => IS_MAC && COLLAPSE_LINE_BACKWARD_MAC(e)
 const COLLAPSE_LINE_FORWARD = e => IS_MAC && COLLAPSE_LINE_FORWARD_MAC(e)
 
-const EXTEND_CHAR_FORWARD = byKey('shift+right')
-const EXTEND_CHAR_BACKWARD = byKey('shift+left')
+const EXTEND_CHAR_FORWARD = isKeyHotkey('shift+right')
+const EXTEND_CHAR_BACKWARD = isKeyHotkey('shift+left')
 
-const EXTEND_LINE_BACKWARD_MAC = byKey('option+shift+up')
-const EXTEND_LINE_FORWARD_MAC = byKey('option+shift+down')
+const EXTEND_LINE_BACKWARD_MAC = isKeyHotkey('option+shift+up')
+const EXTEND_LINE_FORWARD_MAC = isKeyHotkey('option+shift+down')
 const EXTEND_LINE_BACKWARD = e => IS_MAC && EXTEND_LINE_BACKWARD_MAC(e)
 const EXTEND_LINE_FORWARD = e => IS_MAC && EXTEND_LINE_FORWARD_MAC(e)
 
-const UNDO = byKey('mod+z')
-const REDO_MAC = byKey('mod+shift+z')
-const REDO_PC = byKey('mod+y')
+const UNDO = isKeyHotkey('mod+z')
+const REDO_MAC = isKeyHotkey('mod+shift+z')
+const REDO_PC = isKeyHotkey('mod+y')
 const REDO = e => IS_MAC ? REDO_MAC(e) : REDO_PC(e)
 
-const TRANSPOSE_CHARACTER_MAC = byKey('ctrl+t')
+const TRANSPOSE_CHARACTER_MAC = isKeyHotkey('ctrl+t')
 const TRANSPOSE_CHARACTER = e => IS_MAC && TRANSPOSE_CHARACTER_MAC(e)
 
 const CONTENTEDITABLE = e => (
