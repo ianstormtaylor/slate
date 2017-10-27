9da5d348dec11b7460196f658f5471ed0471aa67
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-10-27 09:35:02 -0700

    fix plugins example

--- examples/plugins/index.js
+++ examples/plugins/index.js
@@ -15,7 +15,7 @@ import SoftBreak from 'slate-soft-break'
 
 function WordCount(options) {
   return {
-    render(props) {
+    renderEditor(props) {
       return (
         <div>
           <div>
@@ -56,11 +56,8 @@ class Plugins extends React.Component {
 
   state = {
     state: Plain.deserialize(`This example shows how you can extend Slate with plugins! It uses four fairly simple plugins, but you can use any plugins you want, or write your own!
-
 The first is a simple plugin to collapse the selection whenever the escape key is pressed. Try selecting some text and pressing escape.
-
 The second is another simple plugin that inserts a "soft" break when enter is pressed instead of creating a new block. Try pressing enter!
-
 The third is an example of using the plugin.render property to create a higher-order-component.`)
   }
 
