b00c8a1d0c135dff49bd5b21c959a32e75316d50
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-10-30 09:43:46 -0700

    fix placeholder to not render when composing

--- packages/slate-react/src/plugins/after.js
+++ packages/slate-react/src/plugins/after.js
@@ -24,7 +24,7 @@ import { IS_CHROME, IS_SAFARI } from '../constants/environment'
  * @type {Function}
  */
 
-const debug = Debug('slate:core:after')
+const debug = Debug('slate:after')
 
 /**
  * The after plugin.
@@ -728,6 +728,7 @@ function AfterPlugin() {
 
   function renderPlaceholder(props) {
     const { editor, node } = props
+    if (editor.state.isComposing) return
     if (node.kind != 'block') return
     if (!Text.isTextList(node.nodes)) return
     if (node.text != '') return
