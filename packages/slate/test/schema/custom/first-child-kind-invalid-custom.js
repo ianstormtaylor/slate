c372dd027bf1fe5fe7893adaf4732cfb9cf21388
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-10-31 20:48:57 -0700

    add `first` and `last` validations to schema

---
+++ packages/slate/test/schema/custom/first-child-kind-invalid-custom.js
@@ -0,0 +1,39 @@
+/** @jsx h */
+
+import h from '../../helpers/h'
+
+export const schema = {
+  blocks: {
+    paragraph: {},
+    quote: {
+      first: { kinds: ['block'] },
+      normalize: (change, reason, { child }) => {
+        if (reason == 'first_child_kind_invalid') {
+          change.wrapBlockByKey(child.key, 'paragraph')
+        }
+      }
+    }
+  }
+}
+
+export const input = (
+  <value>
+    <document>
+      <quote>
+        text
+      </quote>
+    </document>
+  </value>
+)
+
+export const output = (
+  <value>
+    <document>
+      <quote>
+        <paragraph>
+          text
+        </paragraph>
+      </quote>
+    </document>
+  </value>
+)
