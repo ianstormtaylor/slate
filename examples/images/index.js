31bde33137b0f2b0d670067992c5e2b8ccaef985
Jeff Lai <winfield301@gmail.com>
2017-11-03 23:29:42 +0800

    Fix lint test

--- examples/images/index.js
+++ examples/images/index.js
@@ -1,5 +1,5 @@
 
-import { Editor, getEventRange, getEventTransfer } from 'slate-react'
+import { Editor, getEventTransfer } from 'slate-react'
 import { Block, Value } from 'slate'
 
 import React from 'react'
