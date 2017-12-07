43bf33e29daeaa609bf92196df1b25de42c12d96
David Silva <zgrav@null.net>
2017-12-07 23:50:08 +0100

    fix(at-range): use select instead of moveToRangeOf

--- packages/slate/src/changes/at-range.js
+++ packages/slate/src/changes/at-range.js
@@ -564,7 +564,7 @@ Changes.deleteForwardAtRange = (change, range, n = 1, options = {}) => {
         focusKey: nextBlock.key,
         focusOffset: 0,
       })
-      change.moveToRangeOf(nextBlock)
+      change.select(range)
     }
     return
   }
