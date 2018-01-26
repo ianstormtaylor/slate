a25020d3b6b936f378441c29c5bc661da94e5178
Zach Schneider <zach@schneid.io>
2018-01-26 18:03:39 -0500

    Convert slate-base64-serializer to rollup

---
+++ packages/slate-base64-serializer/rollup.config.js
@@ -0,0 +1,63 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import builtins from 'rollup-plugin-node-builtins'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import pkg from './package.json'
+
+const umdConfig = {
+  input: 'src/index.js',
+  output: {
+    file: pkg.browser,
+    name: 'slate-base64-serializer',
+    format: 'umd',
+    exports: 'named',
+    globals: {
+      slate: 'Slate',
+    },
+  },
+  external: [
+    'slate',
+  ],
+  plugins: [
+    resolve({
+      preferBuiltins: false,
+    }),
+    commonjs({
+      exclude: ['src/**'],
+    }),
+    builtins(),
+    babel({
+      exclude: ['node_modules/**']
+    }),
+  ]
+}
+
+const umdConfigMin = Object.assign({}, umdConfig)
+umdConfigMin.output = Object.assign({}, umdConfig.output, { file: pkg.browserMin })
+umdConfigMin.plugins = umdConfig.plugins.slice(0).concat(uglify())
+
+export default [
+  // UMD build for browsers
+  umdConfig,
+  umdConfigMin,
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.js',
+    external: [
+      'isomorphic-base64',
+      'slate',
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'named' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: ['node_modules/**']
+      }),
+    ]
+  }
+]
