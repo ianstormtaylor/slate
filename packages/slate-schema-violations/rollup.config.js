f69e64c0a370e10864375c8ce9936c1fc7883a48
Zach Schneider <zach@schneid.io>
2018-01-27 12:58:06 -0500

    Convert slate-schema-violations to rollup

---
+++ packages/slate-schema-violations/rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import pkg from './package.json'
+
+const umdConfig = {
+  input: 'src/index.js',
+  output: {
+    file: pkg.browser,
+    name: 'slate-schema-violations',
+    format: 'umd',
+    exports: 'named',
+  },
+  plugins: [
+    resolve(),
+    commonjs({
+      exclude: ['src/**'],
+    }),
+    babel({
+      include: ['src/**']
+    }),
+  ]
+}
+
+const umdConfigMin = Object.assign({}, umdConfig)
+umdConfigMin.output = Object.assign({}, umdConfig.output, { file: pkg.browserMin })
+umdConfigMin.plugins = umdConfig.plugins.slice(0).concat(uglify())
+
+export default [
+  // UMD build for browsers
+  umdConfig,
+  umdConfigMin,
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'named' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        include: ['src/**']
+      }),
+    ]
+  }
+]
