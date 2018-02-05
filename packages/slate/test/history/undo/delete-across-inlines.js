b8a48c962d2930bb9dfa48a43ee9c88ebdefbcaf
Cameron Ackerman <chackerman@wsu.edu>
2018-02-04 23:09:00 -0800

    partially revert update to test with deletion across inlines

--- packages/slate/test/history/undo/delete-across-inlines.js
+++ packages/slate/test/history/undo/delete-across-inlines.js
@@ -17,6 +17,8 @@ export const input = (
     <document>
       <paragraph>
         <link data={{ url: 'https://www.google.com' }}>o<anchor />ne</link>
+      </paragraph>
+      <paragraph>
         <link data={{ url: 'https://www.github.com' }}>tw<focus />o</link>
       </paragraph>
     </document>
