64d987e5fe4d0d63d96d8533dc9cf192c321017f
zhujinxuan <zhujinxuan@gmail.com>
2018-04-12 17:27:35 -0400

    Rename to slate-dev-benchmark

---
+++ packages/slate-dev-benchmark/src/index.js
@@ -0,0 +1,7 @@
+import Repository from './components/Repository'
+import Suite from './components/Suite'
+import Bench from './components/Bench'
+
+export { Repository, Suite, Bench }
+export default { Repository, Suite, Bench }
+
