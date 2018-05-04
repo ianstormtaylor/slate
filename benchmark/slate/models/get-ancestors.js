aeafc13209f82bbfc8d51ed831319d63b2220c0d
Benjy Cui <benjytrys@gmail.com>
2018-05-04 11:21:48 +0800

    chore: add more benches

---
+++ benchmark/slate/models/get-ancestors.js
@@ -0,0 +1,29 @@
+/** @jsx h */
+/* eslint-disable react/jsx-key */
+
+const h = require('../../helpers/h')
+
+export default function({ value, text }) {
+  value.document.getAncestors(text.key)
+}
+
+const value = (
+  <value>
+    <document>
+      {Array.from(Array(10)).map(() => (
+        <quote>
+          <paragraph>
+            <paragraph>
+              This is editable <b>rich</b> text, <i>much</i> better than a
+              textarea!
+            </paragraph>
+          </paragraph>
+        </quote>
+      ))}
+    </document>
+  </value>
+)
+const text = value.document.getLastText()
+export const input = () => {
+  return { value, text }
+}
