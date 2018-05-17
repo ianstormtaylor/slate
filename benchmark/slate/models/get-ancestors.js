06214043ba4ca76bb3ad5d00a26b72100b24f1c0
zhujinxuan <zhujinxuan@gmail.com>
2018-05-17 16:57:40 -0400

    Fix naive bug

--- benchmark/slate/models/get-ancestors.js
+++ benchmark/slate/models/get-ancestors.js
@@ -3,7 +3,7 @@
 
 const h = require('../../helpers/h')
 
-module.exports.defaulyt = function({ value, text }) {
+module.exports.default = function({ value, text }) {
   value.document.getAncestors(text.key)
 }
 
@@ -24,6 +24,7 @@ const value = (
   </value>
 )
 const text = value.document.getLastText()
+
 module.exports.input = function() {
   return { value, text }
 }
