d2be821c166a5ef8cae216ba9613cbc15b7587e7
zhujinxuan <zhujinxuan@gmail.com>
2018-05-22 14:37:32 -0400

    Add test for get active marks between offsets

---
+++ packages/slate/test/models/text/marks/get-active-marks-between/marked-text-with-totally-different-marks.js
@@ -0,0 +1,16 @@
+/** @jsx h */
+
+import { Set } from 'immutable'
+import h from '../../../../helpers/h'
+
+export const input = (
+  <text>
+    <b>Cat</b>
+    <i> is</i> Cute
+  </text>
+)[0]
+export default function(t) {
+  return t.getActiveMarksBetweenOffsets(0, 6)
+}
+
+export const output = Set()
