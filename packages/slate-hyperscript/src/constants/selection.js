fe6984f34ac12592e0f82d96f00fd6309e2d8805
zhujinxuan <zhujinxuan@gmail.com>
2018-07-04 23:17:08 -0400

    Restructure the slate-hyperscript; I cannot understand and develop on a large all-in-one file

---
+++ packages/slate-hyperscript/src/constants/selection.js
@@ -0,0 +1,11 @@
+/**
+ * Create selection point constants, for comparison by reference.
+ *
+ * @type {Object}
+ */
+
+const ANCHOR = {}
+const CURSOR = {}
+const FOCUS = {}
+
+export { ANCHOR, CURSOR, FOCUS }
