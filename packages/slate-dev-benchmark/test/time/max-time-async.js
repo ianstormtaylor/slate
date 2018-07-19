e0555d35163ef3e1bd105602e42d59ff6412cc4c
zhujinxuan <zhujinxuan@gmail.com>
2018-07-19 16:26:32 -0400

    Change dev-benchmark linting for jest

--- packages/slate-dev-benchmark/test/time/max-time-async.js
+++ packages/slate-dev-benchmark/test/time/max-time-async.js
@@ -9,7 +9,7 @@ let index = 0
 // A wider range than sync, becuase Promise intialization, babel-node takes time
 export function expected() {
   assert(
-    index > 5 && index < 12,
+    index > 4 && index < 15,
     `index should be 10, but is actually ${index}`
   )
   return true
