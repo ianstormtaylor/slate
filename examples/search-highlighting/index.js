ef194afd697c07d715772d61d36a6311063729d1
Ian Storm Taylor <ian@ianstormtaylor.com>
2018-07-27 10:43:47 -0700

    fix atomicness in search example

--- examples/search-highlighting/index.js
+++ examples/search-highlighting/index.js
@@ -130,7 +130,7 @@ class SearchHighlighting extends React.Component {
             focusKey: key,
             focusOffset: offset,
             marks: [{ type: 'highlight' }],
-            atomic: true,
+            isAtomic: true,
           })
         }
 
