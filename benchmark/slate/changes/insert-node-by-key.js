12b8ea7c30a306fad811d011c2570744f89098ec
zhujinxuan <zhujinxuan@gmail.com>
2018-08-16 15:49:02 -0400

    Fix key duplication warning

--- benchmark/slate/changes/insert-node-by-key.js
+++ benchmark/slate/changes/insert-node-by-key.js
@@ -5,18 +5,18 @@ const h = require('../../helpers/h')
 
 module.exports.default = function(change) {
   change
-    .insertNodeByKey('a', 0, <paragraph>Hello world</paragraph>)
-    .insertNodeByKey('a', 1, <paragraph>Hello world</paragraph>)
-    .insertNodeByKey('a', 2, <paragraph>Hello world</paragraph>)
-    .insertNodeByKey('a', 3, <paragraph>Hello world</paragraph>)
-    .insertNodeByKey('a', 4, <paragraph>Hello world</paragraph>)
+    .insertNodeByKey('a0', 0, <paragraph>Hello world</paragraph>)
+    .insertNodeByKey('a1', 1, <paragraph>Hello world</paragraph>)
+    .insertNodeByKey('a2', 2, <paragraph>Hello world</paragraph>)
+    .insertNodeByKey('a3', 3, <paragraph>Hello world</paragraph>)
+    .insertNodeByKey('a4', 4, <paragraph>Hello world</paragraph>)
 }
 
 const value = (
   <value>
     <document>
       {Array.from(Array(10)).map((v, i) => (
-        <quote key="a">
+        <quote key={`a${i}`}>
           <paragraph>
             This is editable <b>rich</b> text, <i>much</i> better than a
             textarea!
