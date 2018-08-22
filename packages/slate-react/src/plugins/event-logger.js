ffdb051be4cd1d71e4ec11d6c98fc8ab41f46e52
zhujinxuan <zhujinxuan@gmail.com>
2018-08-22 17:09:08 -0400

    Annotate

--- packages/slate-react/src/plugins/event-logger.js
+++ packages/slate-react/src/plugins/event-logger.js
@@ -1,7 +1,17 @@
-const isEventLoggerEnabled = window && window.localStorage.ENABLE_EVENT_LOG
+/*
+ * event log is enabled by localStorage
+*/
+
+const IS_EVENT_LOG_ENABLED = window && window.localStorage.ENABLE_EVENT_LOG
+
+/*
+ * Call event.persist() to view event property in devtool
+ * @param {Debug} debug
+ * @return {Debug}
+*/
 
 function eventLogger(debug) {
-  if (!isEventLoggerEnabled) return debug
+  if (!IS_EVENT_LOG_ENABLED) return debug
 
   return function(handlerType, message) {
     if (!message || !debug.enabled) {
