51d18f478b705b9b0ab94fe543bbc329af042ade
Dundercover <dundercover@gmail.com>
2018-10-15 22:43:52 +0200

    Add tests for `Point.isBeforePoint`

---
+++ packages/slate/test/models/point/is-before-point/before-target-path.js
@@ -0,0 +1,20 @@
+import Point from '../../../../src/models/point'
+
+export const input = {
+  point: Point.create({
+    key: 'a',
+    path: [0, 1],
+    offset: 4,
+  }),
+  target: Point.create({
+    key: 'b',
+    path: [0, 2],
+    offset: 2,
+  }),
+}
+
+export default function({ point, target }) {
+  return point.isBeforePoint(target)
+}
+
+export const output = true
