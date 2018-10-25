17429b5b5c6e0dc5de088adfec4c80a67972e031
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2018-10-25 02:10:19 +0200

    Add failing deleteAtRange test

---
+++ packages/slate/test/commands/at-current-range/delete/void-inline-as-last-with-non-void-previous-siblings.js
@@ -0,0 +1,31 @@
+/** @jsx h */
+
+import h from '../../../helpers/h'
+
+export default function(change) {
+  change.delete()
+}
+
+export const input = (
+  <value>
+    <document>
+      <anchor />
+      <paragraph>Hi</paragraph>
+      <paragraph>there</paragraph>
+      <paragraph>
+        <emoji>😊</emoji>
+      </paragraph>
+      <focus />
+    </document>
+  </value>
+)
+
+export const output = (
+  <value>
+    <document>
+      <paragraph>
+        <cursor />
+      </paragraph>
+    </document>
+  </value>
+)
