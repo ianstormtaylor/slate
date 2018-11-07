135f031b66a6c3983f731ba92a626b9ec2a039cb
Dundercover <dundercover@gmail.com>
2018-11-07 11:02:07 +0100

    Add tests for `node.getRootBlocksAtRange`

---
+++ packages/slate/test/models/node/get-root-blocks-at-range/single-void-block.js
@@ -0,0 +1,24 @@
+/** @jsx h */
+
+import { List } from 'immutable'
+import h from '../../../helpers/h'
+
+export const input = (
+  <value>
+    <document>
+      <image key="a" src="https://example.com/image2.png">
+        <text key="b" />
+      </image>
+    </document>
+    <selection isFocused={false}>
+      <anchor key="b" offset={0} />
+      <focus key="b" offset={0} />
+    </selection>
+  </value>
+)
+
+export default function({ document, selection }) {
+  return document.getRootBlocksAtRange(selection).map(n => n.key)
+}
+
+export const output = List(['a'])
