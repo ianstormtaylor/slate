851cefe565bb4f15a5739bcfad61dbd9f55462d1
Dundercover <dundercover@gmail.com>
2018-11-18 23:28:50 +0100

    Add tests for `editor.hasQuery` of React component

---
+++ packages/slate-react/test/components/editor/has-query/missing-plugin.js
@@ -0,0 +1,21 @@
+/** @jsx h */
+
+import Plain from 'slate-plain-serializer'
+
+const defaultValue = Plain.deserialize('')
+
+const plugins = [
+  {
+    queries: {
+      customQuery: () => {},
+    },
+  },
+]
+
+export const input = { defaultValue, plugins }
+
+export default function(editor) {
+  return editor.hasQuery('otherQuery')
+}
+
+export const output = false
