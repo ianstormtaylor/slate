3928ac7c718a4fdb1f4f9290db7d9a8110228840
wuyue <wuyue@teambition.com>
2018-11-26 16:49:54 +0800

    fix(IME): cursor is placed at start of uncommitted string while composing

--- packages/slate-react/src/components/content.js
+++ packages/slate-react/src/components/content.js
@@ -69,6 +69,7 @@ class Content extends React.Component {
 
   tmp = {
     isUpdatingSelection: false,
+    isComposing: false,
   }
 
   /**
@@ -325,6 +326,14 @@ class Content extends React.Component {
   onEvent(handler, event) {
     debug('onEvent', handler)
 
+    if (handler == 'onCompositionStart') {
+      this.tmp.isComposing = true
+    }
+
+    if (handler == 'onCompositionEnd') {
+      window.requestAnimationFrame(() => (this.tmp.isComposing = false))
+    }
+
     // Ignore `onBlur`, `onFocus` and `onSelect` events generated
     // programmatically while updating selection.
     if (
@@ -339,7 +348,7 @@ class Content extends React.Component {
     // cases we don't need to trigger any changes, since our internal model is
     // already up to date, but we do want to update the native selection again
     // to make sure it is in sync. (2017/10/16)
-    if (handler == 'onSelect') {
+    if (handler == 'onSelect' && !this.tmp.isComposing) {
       const { editor } = this.props
       const { value } = editor
       const { selection } = value
