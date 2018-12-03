8ba150d82b79ca68fbc30a3a5f10d16a1afa2110
David Pace <david.pace@domo.com>
2018-12-03 16:42:42 -0700

    Checking to make sure there is only text in the node before removing the whole node.

---
+++ packages/slate/test/commands/at-current-range/delete-backward/empty-after-multiple-void-blocks.js
@@ -0,0 +1,34 @@
+/** @jsx h */
+
+import h from '../../../helpers/h'
+
+export default function(editor) {
+  editor.deleteBackward()
+}
+
+export const input = (
+  <value>
+    <document>
+      <paragraph>
+        <emoji />
+        <emoji />
+        <cursor />
+      </paragraph>
+      <paragraph>
+      </paragraph>
+    </document>
+  </value>
+)
+
+export const output = (
+  <value>
+    <document>
+      <paragraph>
+        <emoji />
+        <cursor />
+      </paragraph>
+      <paragraph>
+      </paragraph>
+    </document>
+  </value>
+)
