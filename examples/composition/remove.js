370264f6479c3776e1ecd2cdc23ce35116a3c807
Sunny Hirai <thesunny@gmail.com>
2019-05-28 16:38:43 -0700

    Changed wording a little

--- examples/composition/remove.js
+++ examples/composition/remove.js
@@ -1,13 +1,13 @@
 import { p, text, bold } from './util'
 
 export default {
-  text: `Select from start to end then press backspace. Move cursor to end. Backspace over all the remaining content.`,
+  text: `Select from ANCHOR to FOCUS then press backspace. Move cursor to end. Backspace over all the remaining content.`,
   document: {
     nodes: [
-      p(bold('Select'), text(' from START and then')),
+      p(text('Go and '), bold('select'), text(' from this ANCHOR and then')),
       p(text('go and select')),
-      p(text('to the END then press '), bold('backspace.')),
-      p(text('After you have done that move selection to very end')),
+      p(text('to this FOCUS then press '), bold('backspace.')),
+      p(text('After you have done that move selection to very end.')),
       p(
         text('Then try '),
         bold('backspacing'),
