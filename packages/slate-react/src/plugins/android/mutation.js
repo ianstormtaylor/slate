20e67b4d14a55b0db5fc1ca2f421718a003b02b8
Sunny Hirai <thesunny@gmail.com>
2019-05-31 14:09:36 -0700

    Cleanup mutation plugin

--- packages/slate-react/src/plugins/android/mutation.js
+++ packages/slate-react/src/plugins/android/mutation.js
@@ -4,12 +4,8 @@ import getWindow from 'get-window'
 import fixSelectionInZeroWidthBlock from './fix-selection-in-zero-width-block'
 import CompositionManager from './composition-manager'
 
-const debug = Debug('slate:mutation-plugin')
-
 function MutationPlugin({ editor }) {
-  let observer
-
-  observer = new CompositionManager(editor)
+  const observer = new CompositionManager(editor)
 
   function onCompositionStart() {
     observer.onCompositionStart()
@@ -26,22 +22,18 @@ function MutationPlugin({ editor }) {
   }
 
   function onComponentDidMount() {
-    debug('onComponentDidMount')
     observer.connect()
   }
 
   function onComponentDidUpdate() {
-    debug('onComponentDidUpdate')
     observer.connect()
   }
 
   function onComponentWillUnmount(event) {
-    debug('onComponentWillUnmount')
     observer.disconnect()
   }
 
   function onRender(event) {
-    debug('onRender')
     observer.disconnect()
 
     // We don't want the `diff` from a previous render to apply to a
