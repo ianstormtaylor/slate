d28f78c0607ad147f85ff4a363fa65d7ddfa7c8c
Guillaume Racicot <gracicot@druide.com>
2019-10-06 09:22:00 -0400

    Mark some events as 'user actions' to prevent scrolling when a programatic selection tranformation is performed (#3019)

--- packages/slate-react/src/components/content.js
+++ packages/slate-react/src/components/content.js
@@ -199,6 +199,7 @@ class Content extends React.Component {
     debug.update('componentDidUpdate')
 
     this.updateSelection()
+    this.props.editor.clearUserActionPerformed()
 
     this.props.onEvent('onComponentDidUpdate')
   }
@@ -342,8 +343,11 @@ class Content extends React.Component {
         native.addRange(range)
       }
 
-      // Scroll to the selection, in case it's out of view.
-      scrollToSelection(native)
+      // Only scroll to selection when a user action is performed
+      if (editor.userActionPerformed() === true) {
+        // Scroll to the selection, in case it's out of view.
+        scrollToSelection(native)
+      }
 
       // Then unset the `isUpdatingSelection` flag after a delay, to ensure that
       // it is still set when selection-related events from updating it fire.
