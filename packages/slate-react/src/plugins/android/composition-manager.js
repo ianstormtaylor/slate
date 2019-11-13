29f4214a47926ab9ece999f2d997cff267b92f8f
Guillaume Racicot <gracicot@druide.com>
2019-11-13 10:43:14 -0500

    Fixed a crash when editing a detached node without any mutations that removes that node

--- packages/slate-react/src/plugins/android/composition-manager.js
+++ packages/slate-react/src/plugins/android/composition-manager.js
@@ -358,7 +358,7 @@ function CompositionManager(editor) {
     const matchCharacterDataParent = () => {
       if (
         firstMutation.target.parentNode &&
-        firstMutation.target.parentNode.closest(`[data-key]`)
+        firstMutation.target.parentNode.closest(`[data-slate-editor]`)
       ) {
         return firstMutation.target.parentNode
       } else {
@@ -372,7 +372,11 @@ function CompositionManager(editor) {
     }
 
     if (firstMutation.type === 'characterData') {
-      resolveDOMNode(matchCharacterDataParent())
+      const parent = matchCharacterDataParent()
+
+      if (parent) {
+        resolveDOMNode(matchCharacterDataParent())
+      }
     } else if (firstMutation.type === 'childList') {
       if (firstMutation.removedNodes.length > 0) {
         if (mutations.length === 1) {
