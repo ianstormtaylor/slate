491e2bbf15a2752ffa616472af7b7c9759a33541
Ian Storm Taylor <ian@ianstormtaylor.com>
2019-11-27 20:20:58 -0500

    update deps, fix prod build

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -6,7 +6,7 @@ import json from 'rollup-plugin-json'
 import replace from 'rollup-plugin-replace'
 import resolve from 'rollup-plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import { startCase } from 'lodash'
 
 import Core from '../../packages/slate/package.json'
