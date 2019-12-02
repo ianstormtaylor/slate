c06f82c5e8b5c242fe8504309b785712afc61184
Ian Storm Taylor <ian@ianstormtaylor.com>
2019-12-02 16:33:55 -0500

    add insertText parent path test

---
+++ packages/slate/test/transforms/insertText/path/block.js
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+
+import { Editor } from 'slate'
+import { jsx } from '../../..'
+
+export const input = (
+  <editor>
+    <block>word</block>
+  </editor>
+)
+
+export const run = editor => {
+  Editor.insertText(editor, 'x', { at: [0] })
+}
+
+export const output = (
+  <editor>
+    <block>x</block>
+  </editor>
+)
