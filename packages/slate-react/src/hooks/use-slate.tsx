d33e33ec28f96e60e95a16dbefc80ccf0c091e63
Ziad Beyens <ziyad.beyens@gmail.com>
2019-12-04 00:24:47 +0100

    fix: ts

--- packages/slate-react/src/hooks/use-slate.tsx
+++ packages/slate-react/src/hooks/use-slate.tsx
@@ -28,7 +28,7 @@ export const SlateContext = createContext<[Editor] | null>(null)
 
 export const Slate = (props: {
   editor: Editor
-  children: JSX.Element | JSX.Element[]
+  children: React.ReactNode
   defaultValue?: Node[]
   onChange?: (children: Node[], operations: Operation[]) => void
 }) => {
