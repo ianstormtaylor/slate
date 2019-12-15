b2d93af56c29711810a40c397b1018ccfc250bf6
Ian Storm Taylor <ian@ianstormtaylor.com>
2019-12-14 21:47:07 -0500

    add extra `Editor.is*` helpers

--- packages/slate/src/interfaces/editor/queries/general.ts
+++ packages/slate/src/interfaces/editor/queries/general.ts
@@ -7,11 +7,12 @@ import {
   Text,
   PathRef,
   PointRef,
+  Element,
+  TextEntry,
   Range,
   RangeRef,
   Node,
 } from '../../..'
-import { TextEntry } from '../../text'
 
 export const NORMALIZING: WeakMap<Editor, boolean> = new WeakMap()
 export const PATH_REFS: WeakMap<Editor, Set<PathRef>> = new WeakMap()
@@ -19,6 +20,14 @@ export const POINT_REFS: WeakMap<Editor, Set<PointRef>> = new WeakMap()
 export const RANGE_REFS: WeakMap<Editor, Set<RangeRef>> = new WeakMap()
 
 export const GeneralQueries = {
+  /**
+   * Check if a value is a block `Element` object.
+   */
+
+  isBlock(editor: Editor, value: any): value is Element {
+    return Element.isElement(value) && !editor.isInline(value)
+  },
+
   /**
    * Check if a value is an `Editor` object.
    */
@@ -40,12 +49,28 @@ export const GeneralQueries = {
   },
 
   /**
+   * Check if a value is an inline `Element` object.
+   */
+
+  isInline(editor: Editor, value: any): value is Element {
+    return Element.isElement(value) && editor.isInline(value)
+  },
+
+  /**
    * Check if the editor is currently normalizing after each operation.
    */
 
   isNormalizing(editor: Editor): boolean {
     const isNormalizing = NORMALIZING.get(editor)
     return isNormalizing === undefined ? true : isNormalizing
+  },
+
+  /**
+   * Check if a value is a void `Element` object.
+   */
+
+  isVoid(editor: Editor, value: any): value is Element {
+    return Element.isElement(value) && editor.isVoid(value)
   },
 
   /**
