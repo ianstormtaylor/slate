7d99c1785b38d9b2dede0bb034feb08820b24dfa
pang <673262625@qq.com>
2020-03-06 14:06:53 +0800

    Update after.js

--- packages/slate-react/src/plugins/dom/after.js
+++ packages/slate-react/src/plugins/dom/after.js
@@ -110,6 +110,7 @@ function AfterPlugin(options = {}) {
       }
 
       case 'insertFromYank':
+      case 'insertFromComposition':
       case 'insertReplacementText':
       case 'insertText': {
         // COMPAT: `data` should have the text for the `insertText` input type
