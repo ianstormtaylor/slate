6e30afa93d69f320c68e2b35fbb192165ff77298
damareyoh <chackerman@wsu.edu>
2020-03-31 21:28:31 -0700

    fix lint error

--- packages/slate-react/src/plugin/react-editor.ts
+++ packages/slate-react/src/plugin/react-editor.ts
@@ -174,7 +174,11 @@ export const ReactEditor = {
 
     return (
       targetEl.closest(`[data-slate-editor]`) === editorEl &&
-      (!editable || targetEl.isContentEditable || !!targetEl.getAttribute('data-slate-zero-width'))
+      (
+        !editable ||
+        targetEl.isContentEditable ||
+        !!targetEl.getAttribute('data-slate-zero-width')
+      )
     )
   },
 
