1763bc0d69f5201973da348a7b3af1146b3c5499
Cameron Ackerman <cameron_ackerman@selinc.com>
2020-04-07 09:14:57 -0700

    fix issue where inline voids can no longer be selected (#3572)
    * fix issue where inline voids can no longer be selected by allowing the zero width space to be an editable target
    
    Co-authored-by: damareyoh <chackerman@wsu.edu>

--- packages/slate-react/src/plugin/react-editor.ts
+++ packages/slate-react/src/plugin/react-editor.ts
@@ -174,7 +174,9 @@ export const ReactEditor = {
 
     return (
       targetEl.closest(`[data-slate-editor]`) === editorEl &&
-      (!editable || targetEl.isContentEditable)
+      (!editable ||
+        targetEl.isContentEditable ||
+        !!targetEl.getAttribute('data-slate-zero-width'))
     )
   },
 
