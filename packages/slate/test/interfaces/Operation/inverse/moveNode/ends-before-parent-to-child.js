b5e5d1740a10a27deb48da6cb39772dace09f2d2
Jason Tamulonis <jason@coda.io>
2020-05-28 09:47:26 -0700

    Add some comments

--- packages/slate/test/interfaces/Operation/inverse/moveNode/ends-before-parent-to-child.js
+++ packages/slate/test/interfaces/Operation/inverse/moveNode/ends-before-parent-to-child.js
@@ -6,4 +6,5 @@ export const test = value => {
   return Operation.inverse(value)
 }
 
+// The path has changed here because the removal of [0, 1] caused [0, 2] to shift forward into its location.
 export const output = { type: 'move_node', path: [0, 1, 1], newPath: [0, 1] }
