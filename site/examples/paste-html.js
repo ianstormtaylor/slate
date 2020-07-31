1f4f37d816e69b1e3f98eca5689f19542cfee6aa
mdmjg <mdj308@nyu.edu>
2020-07-31 15:17:28 -0500

    fixed typo in html file

--- site/examples/paste-html.js
+++ site/examples/paste-html.js
@@ -200,7 +200,7 @@ const Leaf = ({ attributes, children, leaf }) => {
     children = <em>{children}</em>
   }
 
-  if (leaf.underlined) {
+  if (leaf.underline) {
     children = <u>{children}</u>
   }
 
