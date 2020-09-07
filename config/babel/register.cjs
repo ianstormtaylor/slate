623b55a228cbc3a1870d6e6d1cf30ded7d047b8e
LongYinan <lynweklm@gmail.com>
2020-09-08 00:15:12 +0800

    test: migrate to @swc-node/register

--- config/babel/register.cjs
+++
@@ -1,9 +0,0 @@
-require('@babel/register')({
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  inputSourceMap: true,
-  presets: [
-    '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react',
-  ],
-})
