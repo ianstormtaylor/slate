45338139553e599f57bfede995a607925a768297
udayanshevade <udayan.shevade@gmail.com>
2020-09-20 13:51:41 -0700

    add compound test case for getting positions over multiple blocks and inlines

---
+++ packages/slate/test/interfaces/Editor/positions/all/block-multiple-inline-multiple-unit-word.tsx
@@ -0,0 +1,34 @@
+/** @jsx jsx */
+import { Editor } from 'slate'
+import { jsx } from '../../../..'
+
+export const input = (
+  <editor>
+    <block>one</block>
+    <block>
+      two
+      <inline>
+        three
+        <inline>four</inline>
+      </inline>
+      <inline>five</inline>
+    </block>
+    <block>six</block>
+  </editor>
+)
+export const test = editor => {
+  return Array.from(
+    Editor.positions(editor, {
+      at: [],
+      unit: 'word'
+    })
+  )
+}
+export const output = [
+  { path: [0, 0], offset: 0 },
+  { path: [0, 0], offset: 3 },
+  { path: [1, 0], offset: 0 },
+  { path: [1, 2, 0], offset: 4 },
+  { path: [2, 0], offset: 0 },
+  { path: [2, 0], offset: 3 },
+]
