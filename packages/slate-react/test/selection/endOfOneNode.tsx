62bdfb1a67505bbb81ec8b490d8beff1a8d83319
Isaac Schultz <Isaac.Schultz98@gmail.com>
2020-10-29 18:03:42 -0700

    Finished basic test layout for selection

--- packages/slate-react/test/selection/endOfOneNode.tsx
+++ packages/slate-react/test/selection/endOfOneNode.tsx
@@ -4,24 +4,25 @@ import { SlateRange } from 'slate'
 import { mock } from 'jest-mock-extended'
 
 const mockNode1 = mock<DOMNode>()
+const mockNode2 = mock<DOMNode>()
 
 // It appears it is not possible to mock a `Selection`, so we will instead mock the parts of the code that
 // Handle selections. This is not as high coverage, but at least we can test the behavior after getting a selection.
 export const selection: SlateRangeDescription = {
   anchorNode: mockNode1,
-  anchorOffset: 0,
-  focusNode: mockNode1,
-  focusOffset: 2,
+  anchorOffset: 4,
+  focusNode: mockNode2,
+  focusOffset: 1,
   isCollapsed: false,
 }
 
 // Here, we are mocking the output of the 'Editor.toSlatePoint` return from inside `Editor.toSlateRange`
 export const slateRangeSelection: SlateRange = {
-  anchor: { path: [0], offset: 0 },
-  focus: { path: [0], offset: 2 }
+  anchor: { path: [0], offset: 4 },
+  focus: { path: [1], offset: 2 }
 }
 
 export const output = {
-  anchor: { path: [0], offset: 0 },
-  focus: { path: [0], offset: 2 }
+  anchor: { path: [1], offset: 0 },
+  focus: { path: [1], offset: 2 }
 }
--- packages/slate-react/test/selection/twoNode.tsx
+++ packages/slate-react/test/selection/twoNode.tsx
@@ -4,13 +4,14 @@ import { SlateRange } from 'slate'
 import { mock } from 'jest-mock-extended'
 
 const mockNode1 = mock<DOMNode>()
+const mockNode2 = mock<DOMNode>()
 
 // It appears it is not possible to mock a `Selection`, so we will instead mock the parts of the code that
 // Handle selections. This is not as high coverage, but at least we can test the behavior after getting a selection.
 export const selection: SlateRangeDescription = {
   anchorNode: mockNode1,
   anchorOffset: 0,
-  focusNode: mockNode1,
+  focusNode: mockNode2,
   focusOffset: 2,
   isCollapsed: false,
 }
@@ -18,10 +19,10 @@ export const selection: SlateRangeDescription = {
 // Here, we are mocking the output of the 'Editor.toSlatePoint` return from inside `Editor.toSlateRange`
 export const slateRangeSelection: SlateRange = {
   anchor: { path: [0], offset: 0 },
-  focus: { path: [0], offset: 2 }
+  focus: { path: [1], offset: 2 }
 }
 
 export const output = {
   anchor: { path: [0], offset: 0 },
-  focus: { path: [0], offset: 2 }
+  focus: { path: [1], offset: 2 }
 }
