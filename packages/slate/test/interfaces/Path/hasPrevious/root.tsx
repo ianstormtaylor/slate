4c9ad17df882271bd8c7aff55fa4eea5181a3a0d
Mitchell Donaldson <mitch@nod.money>
2020-11-02 17:25:59 +1100

    remove console.log

--- packages/slate/test/interfaces/Path/hasPrevious/root.tsx
+++ packages/slate/test/interfaces/Path/hasPrevious/root.tsx
@@ -2,7 +2,6 @@ import { Path } from 'slate'
 
 export const input = [0, 0]
 export const test = path => {
-  console.log(Path.hasPrevious(path));
   return Path.hasPrevious(path)
 }
 export const output = false;
