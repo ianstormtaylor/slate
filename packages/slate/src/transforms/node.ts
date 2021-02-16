1321bc41ead0ce18f69dce2bc40d3fcbe98a1943
Andrew Herron <thespyder@programmer.net>
2021-02-16 15:03:28 +1000

    Use null instead of undefined as the old value for set_node operations that add a property

--- packages/slate/src/transforms/node.ts
+++ packages/slate/src/transforms/node.ts
@@ -631,7 +631,7 @@ export const NodeTransforms: NodeTransforms = {
           }
 
           if (props[k] !== node[k]) {
-            properties[k] = node[k]
+            properties[k] = node[k] ?? null
             newProperties[k] = props[k]
           }
         }
