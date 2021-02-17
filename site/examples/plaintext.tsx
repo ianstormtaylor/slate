808f14f145027a80957709babf4bdae607291b7d
E-Liang Tan <eliang@eliangtan.com>
2021-02-17 15:16:51 +0800

    Add temporary placeholder example

--- site/examples/plaintext.tsx
+++ site/examples/plaintext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useCallback } from 'react'
 import { Node, createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 import { withHistory } from 'slate-history'
@@ -6,18 +6,24 @@ import { withHistory } from 'slate-history'
 const PlainTextExample = () => {
   const [value, setValue] = useState<Node[]>(initialValue)
   const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+  const [placeholder, setPlaceholder] = useState(1)
+  const newPlaceholder = useCallback(() => setPlaceholder(p => p + 1), [])
+
   return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-      <Editable placeholder="Enter some plain text..." />
-    </Slate>
+    <>
+      <button onClick={newPlaceholder}>New Placeholder</button>
+      <Slate editor={editor} value={value} onChange={setValue}>
+        <Editable placeholder={`${placeholder}`} />
+      </Slate>
+      <div>Actual placeholder value: {placeholder}</div>
+      <p>Placeholder will not update until you type something in the editor</p>
+    </>
   )
 }
 
 const initialValue = [
   {
-    children: [
-      { text: 'This is editable plain text, just like a <textarea>!' },
-    ],
+    children: [{ text: '' }],
   },
 ]
 
