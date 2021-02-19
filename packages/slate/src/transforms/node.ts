df98cfaef91fad0d883bdfd1778e524bc8408a40
Sunny Hirai <thesunny@gmail.com>
2021-02-19 14:44:09 -0800

    Allow null properties in setNodes

--- packages/slate/src/transforms/node.ts
+++ packages/slate/src/transforms/node.ts
@@ -67,7 +67,7 @@ export interface NodeTransforms {
   ) => void
   setNodes: <T extends Node>(
     editor: Editor,
-    props: Partial<Node>,
+    props: Partial<Node> & { [key: string]: null },
     options?: {
       at?: Location
       match?: NodeMatch<T>
