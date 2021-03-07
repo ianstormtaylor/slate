09c4028f13f1796707d1a2ac4562b53781c03ec3
oyzhen <me@oyzhen.me>
2021-03-07 16:54:07 +0800

    feat: add Deno SSR support

--- packages/slate-react/src/hooks/use-isomorphic-layout-effect.ts
+++ packages/slate-react/src/hooks/use-isomorphic-layout-effect.ts
@@ -4,4 +4,6 @@ import { useLayoutEffect, useEffect } from 'react'
  * Prevent warning on SSR by falling back to useEffect when window is not defined
  */
 export const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect
+  typeof window !== 'undefined' && typeof Deno === 'undefined'
+    ? useLayoutEffect
+    : useEffect
