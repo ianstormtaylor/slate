9bcbe2d9ddc21ed99870171ce1a1fbf684b25d15
oyzhen <oyzhen@vip.qq.com>
2021-03-17 09:57:28 +0800

    fix: check if DOM available as React does

--- packages/slate-react/src/utils/environment.ts
+++ packages/slate-react/src/utils/environment.ts
@@ -24,3 +24,13 @@ export const IS_EDGE_LEGACY =
 export const IS_CHROME_LEGACY =
   typeof navigator !== 'undefined' &&
   /Chrome?\/(?:[0-7][0-5]|[0-6][0-9])/i.test(navigator.userAgent)
+
+/**
+ * Check if DOM is available as React does internally.
+ * @see https://github.com/facebook/react/blob/master/packages/shared/ExecutionEnvironment.js
+ */
+export const CAN_USE_DOM = !!(
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined'
+)
