cf42529704695e084b7de71a122e9064ae51b361
Sunny Hirai <thesunny@gmail.com>
2021-03-29 01:06:51 -0700

    Fix linting

--- packages/slate-react/src/plugin/with-react.ts
+++ packages/slate-react/src/plugin/with-react.ts
@@ -8,10 +8,10 @@ import { isDOMText, getPlainText } from '../utils/dom'
 
 /**
  * `withReact` adds React and DOM specific behaviors to the editor.
- * 
+ *
  * If you are using TypeScript, you must extend Slate's CustomTypes to use
  * this plugin.
- * 
+ *
  * See https://docs.slatejs.org/concepts/11-typescript to learn how.
  */
 
