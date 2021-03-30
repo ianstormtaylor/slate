c4bf3aee5afe793eb2f5b91fc25c064c67185c77
Israel Adetunji <israeleadetunji@gmail.com>
2021-03-30 17:49:25 +0100

    Added test files for setNodes

---
+++ packages/slate/test/transforms/setNodes/block/block-function-across.tsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { Editor, Transforms } from 'slate'
+import { jsx } from '../../..'
+
+const updateFunction = props => {
+  return { ...props, key: !props.key }
+}
+
+export const run = editor => {
+  Transforms.setNodes(editor, updateFunction, {
+    match: n => Editor.isBlock(editor, n),
+  })
+}
+
+export const input = (
+  <editor>
+    <block key={false}>
+      <anchor />
+      word
+    </block>
+    <block key={false}>
+      a<focus />
+      nother
+    </block>
+  </editor>
+)
+export const output = (
+  <editor>
+    <block key={true}>
+      <anchor />
+      word
+    </block>
+    <block key={true}>
+      a<focus />
+      nother
+    </block>
+  </editor>
+)
