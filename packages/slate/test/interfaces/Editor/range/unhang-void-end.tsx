b6f943fe69b2f25cd66038c2e40b0b6c1d622d31
Andrew Herron <thespyder@programmer.net>
2021-04-15 11:17:02 +1000

    Added test to ensure unhang does not skip over inline voids at the end of a block

---
+++ packages/slate/test/interfaces/Editor/range/unhang-void-end.tsx
@@ -0,0 +1,29 @@
+/** @jsx jsx */
+import { Editor, Transforms } from 'slate'
+import { jsx } from '../../..'
+
+export const input: Editor = (
+  <editor>
+    <block>
+      one
+      <inline void />
+      <text />
+    </block>
+    <block>two</block>
+  </editor>
+)
+
+export const inputRange = {
+  anchor: { path: [0, 0], offset: 0 },
+  focus: { path: [1, 0], offset: 0 },
+}
+
+export const test = editor => {
+  Transforms.select(editor, inputRange)
+  return Editor.unhangRange(editor, editor.selection)
+}
+
+export const output = {
+  anchor: { path: [0, 0], offset: 0 },
+  focus: { path: [0, 2], offset: 0 },
+}
