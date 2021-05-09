cf6cc2b06b633eee3968f166558112a042c584a5
JonasKruckenberg <iterpre@protonmail.com>
2021-05-09 15:19:04 +0200

    replace lodash/isEqual with fast-deep-equal

--- packages/slate/src/interfaces/text.ts
+++ packages/slate/src/interfaces/text.ts
@@ -1,5 +1,5 @@
 import isPlainObject from 'is-plain-object'
-import isEqual from 'lodash/isEqual'
+import isEqual from 'fast-deep-equal'
 import { Range } from '..'
 import { ExtendedType } from './custom-types'
 
