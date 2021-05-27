7232502d773beea0dd59521cbedb2bc286eaccb6
Kelly Joseph Price <kelly@readme.com>
2021-05-26 17:08:44 -0700

    test: add test case for bug

---
+++ packages/slate/test/transforms/mergeNodes/path/text-hanging.tsx
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+import { Transforms, Text } from 'slate'
+import { jsx } from '../../..'
+
+export const input = (
+  <editor>
+    <block>one</block>
+    <block>
+      <cursor />
+      <text />
+      <text />
+    </block>
+  </editor>
+)
+export const run = editor => {
+  Transforms.mergeNodes(editor, { at: [1], match: Text.isText })
+}
+export const output = (
+  <editor>
+    <block>one</block>
+    <block>
+      <cursor />
+    </block>
+  </editor>
+)
