19c9a8c86cf31f9b2acd3d9d03e0e7c6ca7fb719
Anish Aggarwal <anish17122000@gmail.com>
2021-07-07 18:55:21 +0530

    Added test for 'move-node' operation (#4361)

---
+++ packages/slate/test/operations/move_node/path-not-equals-new-path.tsx
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import { jsx } from 'slate-hyperscript'
+
+export const input = (
+  <editor>
+    <element>1</element>
+    <element>2</element>
+  </editor>
+)
+export const operations = [
+  {
+    type: 'move_node',
+    path: [0],
+    newPath: [1],
+  },
+]
+export const output = (
+  <editor>
+    <element>2</element>
+    <element>1</element>
+  </editor>
+)
