e79f238c7685a321681c1ce5f63e30e2c23a3664
Sunny Hirai <thesunny@gmail.com>
2021-07-08 23:02:02 -0700

    Comment lintstagedrc properly so we know how and why it is there and the process for removing it in the future

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,23 @@
 const escape = require('shell-quote').quote
 
 /**
- * Need this to fix a bug where we can't commit `pages/examples/[example].tsx`.
+ * Need this to fix a bug where we can't commit this:
+ *
+ * `pages/examples/[example].tsx`.
+ *
+ * because of the square brackets `[` and `]`.
  *
  * <https://github.com/okonet/lint-staged/issues/676#issuecomment-574764713>
  *
  * NOTE:
+ *
  * We can remove this entire file if/when we upgrade to Prettier 2+ where this
- * is no longer necessary according to the `lint-staged` issue.
+ * is no longer necessary according to the `lint-staged` issue shown above.
+ *
+ * Currently, the same configuration without the escaping of the filename
+ * still exists in `package.json` but this takes precedence over that.
+ *
+ * Once this file is removed, `package.json` configuration will be used.
  */
 
 module.exports = {
