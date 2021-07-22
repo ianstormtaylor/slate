3145298afe3e0f475de4bcf4c4c42296b2ef700d
Samarjeet <samarsault@gmail.com>
2021-07-22 09:56:57 +0530

    cypress: check html in richtext

--- cypress/integration/richtext.ts
+++ cypress/integration/richtext.ts
@@ -1,7 +1,15 @@
 describe('On richtext example', () => {
-  it('inserts text when typed', () => {
-    cy.visit('examples/richtext')
+  before(() => cy.visit('examples/richtext'))
 
+  it('renders rich text', () => {
+    cy.findByRole('textbox')
+      .get('strong')
+      .should('contain.text', 'rich')
+      .get('blockquote')
+      .should('contain.text', 'wise quote')
+  })
+
+  it('inserts text when typed', () => {
     cy.findByRole('textbox')
       .type('{movetostart}')
       .type('Hello World')
