e0776c5c923f1fb33a130599e558e6dffdde40f4
Dylan Schiemann <dylan@dojotoolkit.org>
2021-08-06 07:43:42 -0700

    Allow setFragmentData without clipboardData object (#4333)
    * allow setFragmentData to be used without a copy/paste or DnD data structure
    
    * Add changeset
    
    * return data for use outside of setFragmentData call

--- packages/slate-react/src/plugin/with-react.ts
+++ packages/slate-react/src/plugin/with-react.ts
@@ -96,7 +96,7 @@ export const withReact = <T extends Editor>(editor: T) => {
     }
   }
 
-  e.setFragmentData = (data: DataTransfer) => {
+  e.setFragmentData = (data: Pick<DataTransfer, 'getData' | 'setData'>) => {
     const { selection } = e
 
     if (!selection) {
@@ -179,6 +179,7 @@ export const withReact = <T extends Editor>(editor: T) => {
     data.setData('text/html', div.innerHTML)
     data.setData('text/plain', getPlainText(div))
     document.body.removeChild(div)
+    return data
   }
 
   e.insertData = (data: DataTransfer) => {
