ianstormtaylor/slate

commit feb293aaa2c02fe3ad319bd021e66908ee770a6e

Apply no-children normalization fix before normalization (#4208)
* Test cases for failure condition

* Before normalizing ensure all elements have at least one text child. This is a normalization requirement that some normalization fixes require, so it must be done as an initial dedicated pass.

packages/slate/src/interfaces/editor.ts
@@ -16,6 +16,7 @@ import {
   RangeRef,
   Span,
   Text,
+  Transforms,
 } from '..'
 import {
   DIRTY_PATHS,
@@ -988,6 +989,27 @@ export const Editor: EditorInterface = {
     }
 
     Editor.withoutNormalizing(editor, () => {
+      /*
+        Fix dirty elements with no children.
+        editor.normalizeNode() does fix this, but some normalization fixes also require it to work.
+        Running an initial pass avoids the catch-22 race condition.
+      */
+      for (const dirtyPath of getDirtyPaths(editor)) {
+        if (Node.has(editor, dirtyPath)) {
+          const [node, _] = Editor.node(editor, dirtyPath)
+
+          // Add a text child to elements with no children.
+          // This is safe to do in any order, by definition it can't cause other paths to change.
+          if (Element.isElement(node) && node.children.length === 0) {
+            const child = { text: '' }
+            Transforms.insertNodes(editor, child, {
+              at: dirtyPath.concat(0),
+              voids: true,
+            })
+          }
+        }
+      }
+
       const max = getDirtyPaths(editor).length * 42 // HACK: better way?
       let m = 0
 

packages/slate/test/transforms/normalization/move_node.tsx
@@ -1,0 +1,21 @@
+/** @jsx jsx */
+import { Transforms } from 'slate'
+import { jsx } from '../..'
+
+export const input = (
+  <editor>
+    <block>one</block>
+    <block>two</block>
+  </editor>
+)
+export const run = editor => {
+  Transforms.moveNodes(editor, { at: [0, 0], to: [1, 0] })
+}
+export const output = (
+  <editor>
+    <block>
+      <text />
+    </block>
+    <block>onetwo</block>
+  </editor>
+)

packages/slate/test/transforms/normalization/split_node-and-insert_node.tsx
@@ -1,0 +1,84 @@
+/** @jsx jsx */
+import { Editor, Transforms } from 'slate'
+import { jsx } from '../..'
+
+export const input = (
+  <editor>
+    <block>
+      <text />
+      <inline>one</inline>
+      <text />
+    </block>
+    <block>
+      <text />
+      <inline>two</inline>
+      <text />
+    </block>
+  </editor>
+)
+export const run = editor => {
+  Editor.withoutNormalizing(editor, () => {
+    const operations = [
+      {
+        type: 'split_node',
+        path: [0, 1],
+        position: 0,
+        properties: { inline: true },
+      },
+      {
+        type: 'split_node',
+        path: [0],
+        position: 1,
+        properties: {},
+      },
+      {
+        type: 'split_node',
+        path: [2, 1, 0],
+        position: 0,
+        properties: {},
+      },
+      {
+        type: 'split_node',
+        path: [2, 1],
+        position: 0,
+        properties: { inline: true },
+      },
+      {
+        type: 'split_node',
+        path: [2],
+        position: 1,
+        properties: {},
+      },
+      { type: 'insert_node', path: [2, 1], node: { text: '' } },
+    ]
+    operations.forEach(editor.apply)
+  })
+}
+export const output = (
+  <editor>
+    <block>
+      <text />
+    </block>
+    <block>
+      <text />
+      <inline>
+        <text />
+      </inline>
+      <text />
+      <inline>one</inline>
+      <text />
+    </block>
+    <block>
+      <text />
+    </block>
+    <block>
+      <text />
+      <inline>
+        <text />
+      </inline>
+      <text />
+      <inline>two</inline>
+      <text />
+    </block>
+  </editor>
+)

commit 09a5e4caa48a13a9abf4bd49a25855ad6eee1b28

Fixed regression in #4208 where normalization on empty block nodes could not be overridden

packages/slate/src/interfaces/editor.ts
@@ -997,16 +997,18 @@ export const Editor: EditorInterface = {
       */
       for (const dirtyPath of getDirtyPaths(editor)) {
         if (Node.has(editor, dirtyPath)) {
-          const [node, _] = Editor.node(editor, dirtyPath)
+          const entry = Editor.node(editor, dirtyPath)
+          const [node, _] = entry
 
-          // Add a text child to elements with no children.
-          // This is safe to do in any order, by definition it can't cause other paths to change.
+          /*
+            The default normalizer inserts an empty text node in this scenario, but it can be customised.
+            So there is some risk here.
+
+            As long as the normalizer only inserts child nodes for this case it is safe to do in any order;
+            by definition adding children to an empty node can't cause other paths to change.
+          */
           if (Element.isElement(node) && node.children.length === 0) {
-            const child = { text: '' }
-            Transforms.insertNodes(editor, child, {
-              at: dirtyPath.concat(0),
-              voids: true,
-            })
+            editor.normalizeNode(entry)
           }
         }
       }

packages/slate/test/normalization/block/insert-custom-block.tsx
@@ -1,0 +1,40 @@
+/** @jsx jsx */
+import { jsx } from '../..'
+import { Editor, Element, Transforms } from 'slate'
+
+export const input = (
+  <editor>
+    <element type="body" />
+  </editor>
+)
+
+// patch in a custom normalizer that inserts empty paragraphs in the body instead of text nodes
+// this test also verifies the new node itself is also normalized, because it's inserting a non-normalized node
+const editor = (input as unknown) as Editor
+const defaultNormalize = editor.normalizeNode
+editor.normalizeNode = entry => {
+  const [node, path] = entry
+  if (
+    Element.isElement(node) &&
+    node.children.length === 0 &&
+    (node as any).type === 'body'
+  ) {
+    const child = { type: 'paragraph', children: [] }
+    Transforms.insertNodes(editor, child, {
+      at: path.concat(0),
+      voids: true,
+    })
+  } else {
+    defaultNormalize(entry)
+  }
+}
+
+export const output = (
+  <editor>
+    <element type="body">
+      <element type="paragraph">
+        <text />
+      </element>
+    </element>
+  </editor>
+)