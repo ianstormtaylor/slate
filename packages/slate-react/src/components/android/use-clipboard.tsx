7cf6d87b42d024807fb34ade945dcfa8aff5a628
surya darma <budisuryadarma0407@gmail.com>
2021-08-12 16:40:30 +0800

    get the fragment from text/html when it missing

--- packages/slate-react/src/components/android/use-clipboard.tsx
+++ packages/slate-react/src/components/android/use-clipboard.tsx
@@ -1,33 +1,27 @@
-import { useRef, useCallback } from 'react'
+import { useCallback } from 'react'
+
+const catchSlateFragment = /data-slate-fragment="(.+?)"/m
 
 const useClipboard = () => {
-  const clipboardDataRef = useRef<DataTransfer | null>(null)
-  const setData = useCallback((nativeDataTransfer: DataTransfer): void => {
-    const clipboard = clipboardDataRef.current || new DataTransfer()
-    nativeDataTransfer.types.forEach(type => {
-      clipboard.setData(type, nativeDataTransfer.getData(type))
-    })
-    clipboardDataRef.current = clipboard
-  }, [])
   const getData = useCallback(
     (nativeDataTransfer: DataTransfer): DataTransfer => {
-      if (clipboardDataRef.current) {
-        const clipboardRef = clipboardDataRef.current
-        const refPlain = clipboardRef.getData('text/plain')
-        const nativePlain = nativeDataTransfer.getData('text/plain')
-        if (nativePlain === refPlain) {
-          /*
-           * if the plain was same, assume the clipboard is still same
-           */
-          return clipboardDataRef.current
+      if (!nativeDataTransfer.getData('application/x-slate-fragment')) {
+        const htmlData = nativeDataTransfer.getData('text/html')
+        const [, fragment] = htmlData.match(catchSlateFragment) || []
+        if (fragment) {
+          const clipboardData = new DataTransfer()
+          nativeDataTransfer.types.forEach(type => {
+            clipboardData.setData(type, nativeDataTransfer.getData(type))
+          })
+          clipboardData.setData('application/x-slate-fragment', fragment)
+          return clipboardData
         }
-        clipboardDataRef.current = null
       }
       return nativeDataTransfer
     },
     []
   )
-  return { setData, getData }
+  return { getData }
 }
 
 export default useClipboard
