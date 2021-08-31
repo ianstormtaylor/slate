b1e3bee3d2d051eb4f81e29a7dec61695f2a3b78
Jokcy <jokcylou@hotmail.com>
2021-08-31 21:27:27 +0800

    fix cursor not correct when insert multiple nodes using insertNodes

---
+++ packages/slate/test/transforms/insertNodes/path/multiple-inline-not-end.tsx
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+import { Transforms } from 'slate'
+import { jsx } from '../../..'
+
+export const input = (
+  <editor>
+    <block>
+      hel
+      <cursor />
+      lo
+    </block>
+  </editor>
+)
+export const run = editor => {
+  Transforms.insertNodes(
+    editor,
+    [
+      <inline>
+        <text />
+      </inline>,
+      <text>world</text>
+    ],
+  )
+}
+export const output = (
+  <editor>
+    <block>
+      hel
+      <inline>
+        <text />
+      </inline>
+      world
+      <cursor />
+      lo
+    </block>
+  </editor>
+)
