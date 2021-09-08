5a98f920c8bd27596e289addfb3de84e1e645f01
Tom Scott <tscott@nerdstgamers.com>
2021-09-08 13:21:34 -0400

    Upgrade `is-plain-object` to v5.0.0
    The `is-plain-object` package recently had a major version upgrade that
    broke libraries which import its default export, such as this one. This
    causes issues when other packages in the same application require a
    higher version of `is-plain-object`, resulting in an error originating
    in Slate's codebase. To remedy this, Slate is now depending on
    `is-plain-object@^5.0.0` and its import references across the codebase
    have been updated.
    
    Fixes #4499

--- packages/slate/src/interfaces/point.ts
+++ packages/slate/src/interfaces/point.ts
@@ -1,4 +1,4 @@
-import isPlainObject from 'is-plain-object'
+import { isPlainObject } from 'is-plain-object'
 import { produce } from 'immer'
 import { ExtendedType, Operation, Path } from '..'
 
--- packages/slate/src/utils/deep-equal.ts
+++ packages/slate/src/utils/deep-equal.ts
@@ -1,4 +1,4 @@
-import isPlainObject from 'is-plain-object'
+import { isPlainObject } from 'is-plain-object'
 
 /*
   Custom deep equal comparison for Slate nodes.
