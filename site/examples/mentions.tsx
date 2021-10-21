ec71aa201e87f4b04823c06d013c926a86ba5058
Jim Fisher <jameshfisher@gmail.com>
2021-10-21 18:46:34 +0100

    Add 'image delete' feature to example
    My immediate motivation is to demonstrate the bug that this fixes. But
    this is also a very common editor feature, and I think it's valuable
    to show how to achieve it.

--- site/examples/mentions.tsx
+++ site/examples/mentions.tsx
@@ -8,6 +8,7 @@ import {
   withReact,
   useSelected,
   useFocused,
+  useEditor,
 } from 'slate-react'
 
 import { Portal } from '../components'
@@ -176,6 +177,9 @@ const Element = props => {
 }
 
 const Mention = ({ attributes, children, element }) => {
+  const editor = useEditor()
+  const path = ReactEditor.findPath(editor, element)
+
   const selected = useSelected()
   const focused = useFocused()
   return (
@@ -196,6 +200,10 @@ const Mention = ({ attributes, children, element }) => {
     >
       @{element.character}
       {children}
+      <button onClick={() => {
+        // Remove myself from the Slate document
+        Transforms.removeNodes(editor, { at: path })
+      }}>X</button>
     </span>
   )
 }
