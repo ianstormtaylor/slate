a6eddbdbbd8078e705756162e74ddefa823d9369
Dylan Schiemann <dylan@dojotoolkit.org>
2021-12-05 21:26:18 +0000

    Update site/components.tsx

--- site/components.tsx
+++ site/components.tsx
@@ -1,6 +1,6 @@
 import React, { Ref, PropsWithChildren } from 'react'
 import ReactDOM from 'react-dom'
-import { css, cx } from @emotion/css';
+import { css, cx } from '@emotion/css';
 
 interface BaseProps {
   className: string
