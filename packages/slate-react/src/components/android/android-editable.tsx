106e1aa5bf47e569b1b4177ed2905e16034899cb
Alessio Galdy <alessio.galdy@gmail.com>
2022-01-08 23:40:17 +0100

    * remove scheduling of text updates using setTimeout * call Transforms.setSelection before Editor.insertText

--- packages/slate-react/src/components/android/android-editable.tsx
+++ packages/slate-react/src/components/android/android-editable.tsx
@@ -24,11 +24,7 @@ import {
   IS_READ_ONLY,
   NODE_TO_ELEMENT,
   PLACEHOLDER_SYMBOL,
-  IS_COMPOSING,
-  IS_ON_COMPOSITION_END,
-  EDITOR_ON_COMPOSITION_TEXT,
 } from '../../utils/weak-maps'
-import { normalizeTextInsertionRange } from './diff-text'
 
 import { EditableProps, hasTarget } from '../editable'
 import useChildren from '../../hooks/use-children'
@@ -524,46 +520,6 @@ export const AndroidEditable = (props: EditableProps): JSX.Element => {
                 !isEventHandled(event, attributes.onCompositionEnd)
               ) {
                 scheduleOnDOMSelectionChange.flush()
-                setTimeout(() => {
-                  state.isComposing && setIsComposing(false)
-                  state.isComposing = false
-
-                  IS_COMPOSING.set(editor, false)
-                  IS_ON_COMPOSITION_END.set(editor, true)
-
-                  const insertedText =
-                    EDITOR_ON_COMPOSITION_TEXT.get(editor) || []
-
-                  // `insertedText` is set in `MutationObserver` constructor.
-                  // If open phone keyboard association function, `CompositionEvent` will be triggered.
-                  if (!insertedText.length) {
-                    return
-                  }
-
-                  EDITOR_ON_COMPOSITION_TEXT.set(editor, [])
-
-                  const { selection, marks } = editor
-
-                  insertedText.forEach(insertion => {
-                    const text = insertion.text.insertText
-                    const at = normalizeTextInsertionRange(
-                      editor,
-                      selection,
-                      insertion
-                    )
-                    if (marks) {
-                      const node = { text, ...marks }
-                      Transforms.insertNodes(editor, node, {
-                        match: Text.isText,
-                        at,
-                        select: true,
-                      })
-                      editor.marks = null
-                    } else {
-                      Editor.insertText(editor, text)
-                    }
-                  })
-                }, RESOLVE_DELAY)
               }
             },
             [attributes.onCompositionEnd]
@@ -576,7 +532,6 @@ export const AndroidEditable = (props: EditableProps): JSX.Element => {
               ) {
                 !state.isComposing && setIsComposing(true)
                 state.isComposing = true
-                IS_COMPOSING.set(editor, true)
               }
             },
             [attributes.onCompositionUpdate]
@@ -589,7 +544,6 @@ export const AndroidEditable = (props: EditableProps): JSX.Element => {
               ) {
                 !state.isComposing && setIsComposing(true)
                 state.isComposing = true
-                IS_COMPOSING.set(editor, true)
               }
             },
             [attributes.onCompositionStart]
