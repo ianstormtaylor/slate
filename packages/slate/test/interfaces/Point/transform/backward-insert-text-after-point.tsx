e03850e2278b1e6ca08739ac74d4cb82b5ba48d7
rockettomatooo <samustecher99@gmail.com>
2022-02-21 18:26:47 +0100

    fix point transform for insert_text

---
+++ packages/slate/test/interfaces/Point/transform/backward-insert-text-after-point.tsx
@@ -0,0 +1,24 @@
+import { Point } from 'slate'
+
+export const input = {
+  path: [0, 0],
+  offset: 0,
+}
+
+export const test = value => {
+  return Point.transform(
+    value,
+    {
+      type: 'insert_text',
+      path: [0, 0],
+      text: 'a',
+      offset: 1,
+      properties: {},
+    },
+    { affinity: 'backward' }
+  )
+}
+export const output = {
+  path: [0, 0],
+  offset: 0,
+}
