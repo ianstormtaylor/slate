c04258a0ac910438c831379f7eab0811f013ef1e
Horacio Herrera <hi@horacioh.com>
2022-04-01 16:30:01 +0200

    update cypress
    - move cypress folder

--- site/next.config.js
+++ site/next.config.js
@@ -2,6 +2,9 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   webpack: config => {
     config.module.rules.push({
       test: /\.js$/,
