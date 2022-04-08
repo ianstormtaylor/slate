c0024f5a899ba61acb4e64a1b126cd353a8db514
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-04-08 17:58:23 +0300

    Update index.ts

--- packages/slate-hyperscript/src/index.ts
+++ packages/slate-hyperscript/src/index.ts
@@ -3,7 +3,10 @@ import {
   HyperscriptCreators,
   HyperscriptShorthands,
 } from './hyperscript'
-import { createEditor } from './creators'
+import { 
+  createEditor, 
+  createText,
+} from './creators'
 
 /**
  * The default hyperscript factory that ships with Slate, without custom tags.
@@ -15,6 +18,7 @@ export {
   jsx,
   createHyperscript,
   createEditor,
+  createText,
   HyperscriptCreators,
   HyperscriptShorthands,
 }
