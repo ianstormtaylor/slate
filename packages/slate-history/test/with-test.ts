69ed18dd9cae49dd309765558428fdb631fa4b40
Jonas Kruckenberg <iterpre@protonmail.com>
2022-04-10 12:34:26 +0200

    Create with-test.ts

---
+++ packages/slate-history/test/with-test.ts
@@ -0,0 +1,10 @@
+export const withTest = editor => {
+  const { isInline, isVoid } = editor
+  editor.isInline = element => {
+    return element.inline === true ? true : isInline(element)
+  }
+  editor.isVoid = element => {
+    return element.void === true ? true : isVoid(element)
+  }
+  return editor
+}
