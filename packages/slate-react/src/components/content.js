8e654f891237be3bb3466df940e0f8ce353abb23
Florian Nourrisse <florian@nourrisse.com>
2022-09-06 14:30:56 +0200

    fix(slate): fix an issue with chromium 105+

--- packages/slate-react/src/components/content.js
+++ packages/slate-react/src/components/content.js
@@ -507,7 +507,11 @@ class Content extends React.Component {
       // COMPAT: In iOS, a formatting menu with bold, italic and underline
       // buttons is shown which causes our internal value to get out of sync in
       // weird ways. This hides that. (2016/06/21)
-      ...(readOnly ? {} : { WebkitUserModify: 'read-write-plaintext-only' }),
+      /* 
+        09/2022: Following an issue with chromium 105+, this line has to be modified
+        Credits to @zarv1k https://github.com/ianstormtaylor/slate/issues/5110#issuecomment-1234951122 
+        */
+      ...(readOnly ? {} : { WebkitUserModify: 'read-write' }),
       // Allow for passed-in styles to override anything.
       ...props.style,
     }
