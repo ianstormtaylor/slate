5fff927154613c0495144735240588a716d18c4c
Laufey <laufeyrut@gmail.com>
2022-09-13 13:41:59 +0000

    Make it possible to delete block elements with backspace in chrome and safari

--- packages/slate-react/src/components/editable.tsx
+++ packages/slate-react/src/components/editable.tsx
@@ -1555,7 +1555,8 @@ export const Editable = (props: EditableProps) => {
                         if (
                           Element.isElement(currentNode) &&
                           Editor.isVoid(editor, currentNode) &&
-                          Editor.isInline(editor, currentNode)
+                          (Editor.isInline(editor, currentNode) ||
+                            Editor.isBlock(editor, currentNode))
                         ) {
                           event.preventDefault()
                           Editor.deleteBackward(editor, { unit: 'block' })
