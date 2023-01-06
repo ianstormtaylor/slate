11f44ec4aabc21b115704dfbe073d34c65c9db22
Gary Borton <gdborton@gmail.com>
2023-01-06 00:20:31 -0800

    Clean up playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
-import os from 'os';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
 
 const projects = [
   {
@@ -18,10 +11,6 @@ const projects = [
         // headless: false,
         /**
          * Enable scrollbars in headless mode.
-         * This is needed to successfully write a test for this issue:
-         * https://github.com/vercel/next-live-mode/issues/1259
-         * We should manually crop out the scrollbar when/if we want to take
-         * a screenshot of the whole page for visual diffing.
          */
         ignoreDefaultArgs: ['--hide-scrollbars'],
       },
@@ -44,15 +33,11 @@ const projects = [
 //   });
 // }
 
-const retries = process.env.LIVE_PLAYWRIGHT_RETRIES
-  ? +process.env.LIVE_PLAYWRIGHT_RETRIES
+const retries = process.env.PLAYWRIGHT_RETRIES
+  ? +process.env.PLAYWRIGHT_RETRIES
   : process.env.CI
   ? 5
   : 0;
-
-if (retries) {
-  console.log(`Running tests with retries: ${retries}`);
-}
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -73,7 +58,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // allow LIVE_PLAYWRIGHT_RETRIES to override for local dev
+  // allow PLAYWRIGHT_RETRIES to override for local dev
   retries,
   /* Opt out of parallel tests. */
   // workers: 1,
@@ -99,13 +84,6 @@ const config: PlaywrightTestConfig = {
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
-
-  /*webServer: {
-    command: 'cd ../../ && pnpm run dev',
-    url: 'http://localhost:9999',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },*/
 };
 
 export default config;
