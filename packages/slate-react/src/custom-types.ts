b49ac5624b857ce7cb7cc7f875f5d9522a88e766
Sergey Dedkov <dsv.mail@yandex.ru>
2023-01-26 01:07:41 +0600

    add basePath prop to Range inside slate-react custom types, calculate absolute ranges on passing them into TextComponent

--- packages/slate-react/src/custom-types.ts
+++ packages/slate-react/src/custom-types.ts
@@ -1,4 +1,4 @@
-import { BaseRange, BaseText } from 'slate'
+import { BaseRange, BaseText, Path } from 'slate'
 import { ReactEditor } from './plugin/react-editor'
 
 declare module 'slate' {
@@ -8,6 +8,7 @@ declare module 'slate' {
       placeholder?: string
     }
     Range: BaseRange & {
+      basePath?: Path
       placeholder?: string
     }
   }
