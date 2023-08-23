ef1de027854df09a2bb3c2c70f1b99be4278c74c
Dylan Schiemann <dylan@dojotoolkit.org>
2023-08-23 04:45:28 -0700

    Update packages/slate-react/src/utils/environment.ts

--- packages/slate-react/src/utils/environment.ts
+++ packages/slate-react/src/utils/environment.ts
@@ -54,7 +54,8 @@ export const IS_UC_MOBILE =
 
 // Wechat browser (not including mac wechat)
 export const IS_WECHATBROWSER =
-  typeof navigator !== 'undefined' && /.*(?<!Mac)Wechat/.test(navigator.userAgent)
+  typeof navigator !== 'undefined' &&
+  /.*(?<!Mac)Wechat/.test(navigator.userAgent)
 
 // Check if DOM is available as React does internally.
 // https://github.com/facebook/react/blob/master/packages/shared/ExecutionEnvironment.js
