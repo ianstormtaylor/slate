5dfbbc56b84a0e9f263f340317f45d85db5c691b
Sebastian <seb@boople.ai>
2025-01-27 16:32:54 +1000

    useRef has initial value, updated types

--- packages/slate-react/src/hooks/use-mutation-observer.ts
+++ packages/slate-react/src/hooks/use-mutation-observer.ts
@@ -2,7 +2,7 @@ import { RefObject, useEffect, useState } from 'react'
 import { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect'
 
 export function useMutationObserver(
-  node: RefObject<HTMLElement>,
+  node: RefObject<HTMLElement | null>,
   callback: MutationCallback,
   options: MutationObserverInit
 ) {
