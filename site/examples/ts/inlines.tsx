8116631873deaecde820efa0b5ea02357dc81ad7
Felix Feng <felixfeng33@gmail.com>
2025-05-28 15:12:51 +0800

    fix

--- site/examples/ts/inlines.tsx
+++ site/examples/ts/inlines.tsx
@@ -85,6 +85,30 @@ const InlinesExample = () => {
     []
   )
 
+  const onKeyDown: React.KeyboardEventHandler<HTMLInputElement> = event => {
+    const { selection } = editor
+
+    // Default left/right behavior is unit:'character'.
+    // This fails to distinguish between two cursor positions, such as
+    // <inline>foo<cursor/></inline> vs <inline>foo</inline><cursor/>.
+    // Here we modify the behavior to unit:'offset'.
+    // This lets the user step into and out of the inline without stepping over characters.
+    // You may wish to customize this further to only use unit:'offset' in specific cases.
+    if (selection && Range.isCollapsed(selection)) {
+      const { nativeEvent } = event
+      if (isKeyHotkey('left', nativeEvent)) {
+        event.preventDefault()
+        Transforms.move(editor, { unit: 'offset', reverse: true })
+        return
+      }
+      if (isKeyHotkey('right', nativeEvent)) {
+        event.preventDefault()
+        Transforms.move(editor, { unit: 'offset' })
+        return
+      }
+    }
+  }
+
   return (
     <SlateReact.Slate editor={editor} initialValue={initialValue}>
       <Toolbar>
@@ -96,6 +120,7 @@ const InlinesExample = () => {
         renderElement={props => <Element {...props} />}
         renderLeaf={props => <Text {...props} />}
         placeholder="Enter some text..."
+        onKeyDown={onKeyDown}
       />
     </SlateReact.Slate>
   )
