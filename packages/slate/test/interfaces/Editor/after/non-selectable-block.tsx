e3bad268e27db6c11712d1b413e176c72e450132
Joe Anderson <joe@mousetrapped.co.uk>
2025-08-04 14:56:43 +0100

    Fix handling of `editor.isSelectable` in `Editor.positions`

---
+++ packages/slate/test/interfaces/Editor/after/non-selectable-block.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+
+import { Editor } from 'slate'
+import { jsx } from '../../..'
+
+export const input = (
+  <editor>
+    <block>one</block>
+    <block nonSelectable>two</block>
+    <block>three</block>
+  </editor>
+)
+
+export const test = editor => {
+  return Editor.after(editor, { path: [0, 0], offset: 3 })
+}
+
+export const output = { path: [2, 0], offset: 0 }
