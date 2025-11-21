addf0c5128bd48693f86a2c97cea133b66dd87e6
nabbydude <nabbydude@gmail.com>
2025-11-21 14:55:45 -0500

    Remove immer, making the core `slate` package dependency-free (#5971)
    * extracted modify functions into util file for reuse
    
    * changed type signature to work with any ancestor root node
    
    * switched from immer to existing modify functions
    
    * removed dependencies on immer and (unused) tiny-warning
    
    * changeset

---
+++ packages/slate/src/utils/modify.ts
@@ -0,0 +1,99 @@
+import {
+  Ancestor,
+  Descendant,
+  Element,
+  Node,
+  Path,
+  Scrubber,
+  Text,
+} from '../interfaces'
+
+export const insertChildren = <T>(
+  xs: T[],
+  index: number,
+  ...newValues: T[]
+) => [...xs.slice(0, index), ...newValues, ...xs.slice(index)]
+
+export const replaceChildren = <T>(
+  xs: T[],
+  index: number,
+  removeCount: number,
+  ...newValues: T[]
+) => [...xs.slice(0, index), ...newValues, ...xs.slice(index + removeCount)]
+
+export const removeChildren = replaceChildren
+
+/**
+ * Replace a descendant with a new node, replacing all ancestors
+ */
+export const modifyDescendant = <N extends Descendant>(
+  root: Ancestor,
+  path: Path,
+  f: (node: N) => N
+) => {
+  if (path.length === 0) {
+    throw new Error('Cannot modify the editor')
+  }
+
+  const node = Node.get(root, path) as N
+  const slicedPath = path.slice()
+  let modifiedNode: Node = f(node)
+
+  while (slicedPath.length > 1) {
+    const index = slicedPath.pop()!
+    const ancestorNode = Node.get(root, slicedPath) as Ancestor
+
+    modifiedNode = {
+      ...ancestorNode,
+      children: replaceChildren(ancestorNode.children, index, 1, modifiedNode),
+    }
+  }
+
+  const index = slicedPath.pop()!
+  root.children = replaceChildren(root.children, index, 1, modifiedNode)
+}
+
+/**
+ * Replace the children of a node, replacing all ancestors
+ */
+export const modifyChildren = (
+  root: Ancestor,
+  path: Path,
+  f: (children: Descendant[]) => Descendant[]
+) => {
+  if (path.length === 0) {
+    root.children = f(root.children)
+  } else {
+    modifyDescendant<Element>(root, path, node => {
+      if (Text.isText(node)) {
+        throw new Error(
+          `Cannot get the element at path [${path}] because it refers to a leaf node: ${Scrubber.stringify(
+            node
+          )}`
+        )
+      }
+
+      return { ...node, children: f(node.children) }
+    })
+  }
+}
+
+/**
+ * Replace a leaf, replacing all ancestors
+ */
+export const modifyLeaf = (
+  root: Ancestor,
+  path: Path,
+  f: (leaf: Text) => Text
+) =>
+  modifyDescendant(root, path, node => {
+    if (!Text.isText(node)) {
+      throw new Error(
+        `Cannot get the leaf node at path [${path}] because it refers to a non-leaf node: ${Scrubber.stringify(
+          node
+        )}`
+      )
+    }
+
+    return f(node)
+  })
