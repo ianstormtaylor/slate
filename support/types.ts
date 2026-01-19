4e50cd30a9a8cff52efd067dc2e4a956f972ca02
nabbydude <nabbydude@gmail.com>
2026-01-19 14:42:02 -0500

    lint + include react tests in linting + disable lib typechecks

--- support/types.ts
+++ support/types.ts
@@ -30,7 +30,7 @@ export interface ElementFlags {
   nonSelectable?: true
 }
 
-// eslint-disable-next-line prettier/prettier
+// prettier-ignore
 type singleLetter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
 
 type SingleLetterFlags = {
