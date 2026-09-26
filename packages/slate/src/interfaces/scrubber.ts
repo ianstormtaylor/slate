08c69e9b5fd4c2e35de268a04e56e02dcb758a65
nabbydude <nabbydude@gmail.com>
2026-09-25 20:23:23 -0400

    add scubber api metadata

--- packages/slate/src/interfaces/scrubber.ts
+++ packages/slate/src/interfaces/scrubber.ts
@@ -1,11 +1,12 @@
+/**
+ * Used by {@link ScrubberInterface} to scrub sensitive data from objects before they are stringified and logged.
+ * it is run for each key of each object recursively. The top level object has a key of `""`.
+ *
+ * @param key The key of the current property being scrubbed.
+ * @param value The value of the current property being scrubbed.
+ * @returns The scrubbed value. If the value is an object, it will be recursively scrubbed.
+ */
 export type Scrubber = (key: string, value: unknown) => unknown
-
-export interface ScrubberInterface {
-  setScrubber(scrubber: Scrubber | undefined): void
-  stringify(value: any): string
-}
-
-let _scrubber: Scrubber | undefined = undefined
 
 /**
  * This interface implements a stringify() function, which is used by Slate
@@ -22,6 +23,22 @@ let _scrubber: Scrubber | undefined = undefined
  *    });
  *
  */
+export interface ScrubberInterface {
+  /**
+   * Set the scrubber function.
+   * @param scrubber The scrubber function to use on inputs of {@link Scrubber.stringify} and each of its properties, or `undefined` for no scrubbing
+   */
+  setScrubber(scrubber: Scrubber | undefined): void
+  /**
+   * Convert a value into a JSON string, using the scrubber function if one has been set.
+   * @param value The value to stringify
+   * @returns The JSON string representation of the value
+   */
+  stringify(value: any): string
+}
+
+let _scrubber: Scrubber | undefined = undefined
+
 // eslint-disable-next-line no-redeclare
 export const Scrubber: ScrubberInterface = {
   setScrubber(scrubber: Scrubber | undefined): void {
